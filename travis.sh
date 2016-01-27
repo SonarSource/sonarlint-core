c9605b83ccc760ce9f3bc64368783f857144fd4c
Julien HENRY <julien.henry@sonarsource.com>
2016-01-27 14:06:00 +0100

    Enable Travis build

---
+++ travis.sh
@@ -0,0 +1,60 @@
+#!/bin/bash
+
+set -euo pipefail
+
+function installTravisTools {
+  mkdir ~/.local
+  curl -sSL https://github.com/SonarSource/travis-utils/tarball/v21 | tar zx --strip-components 1 -C ~/.local
+  source ~/.local/bin/install
+}
+
+function strongEcho {
+  echo ""
+  echo "================ $1 ================="
+}
+
+
+case "$TARGET" in
+
+CI)
+  if [ "$TRAVIS_PULL_REQUEST" != "false" ] && [ "$TRAVIS_BRANCH" == "master" ] && [ "$TRAVIS_SECURE_ENV_VARS" == "true" ]; then
+    strongEcho 'Build and analyze commit in master'
+    # this commit is master must be built and analyzed (with upload of report)
+    export MAVEN_OPTS="-Xmx1G -Xms128m"
+    mvn org.jacoco:jacoco-maven-plugin:prepare-agent verify sonar:sonar \
+      -Pcoverage-per-test \
+      -Dmaven.test.redirectTestOutputToFile=false \
+      -Dsonar.host.url=$SONAR_HOST_URL \
+      -Dsonar.login=$SONAR_TOKEN \
+      -B -e -V
+
+  elif [ "$TRAVIS_PULL_REQUEST" != "false" ] && [ "$TRAVIS_SECURE_ENV_VARS" == "true" ]; then
+    strongEcho 'Build and analyze pull request'
+    # this pull request must be built and analyzed (without upload of report)
+    mvn org.jacoco:jacoco-maven-plugin:prepare-agent verify sonar:sonar \
+      -Pcoverage-per-test -Dmaven.test.redirectTestOutputToFile=false \
+      -Dsonar.analysis.mode=issues \
+      -Dsonar.github.pullRequest=$TRAVIS_PULL_REQUEST \
+      -Dsonar.github.repository=$TRAVIS_REPO_SLUG \
+      -Dsonar.github.oauth=$GITHUB_TOKEN \
+      -Dsonar.host.url=$SONAR_HOST_URL \
+      -Dsonar.login=$SONAR_TOKEN \
+      -B -e -V
+
+  else
+    strongEcho 'Build, no analysis'
+    # Build branch, without any analysis
+
+    # No need for Maven goal "install" as the generated JAR file does not need to be installed
+    # in Maven local repository
+    mvn verify -Dmaven.test.redirectTestOutputToFile=false -B -e -V
+  fi
+  ;;
+
+
+*)
+  echo "Unexpected TARGET value: $TARGET"
+  exit 1
+  ;;
+
+esac
