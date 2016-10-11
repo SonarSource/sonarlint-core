188e97cebc781ae5cbc3406d639fae9934b99a64
Janos Gyerik <janos.gyerik@sonarsource.com>
2016-10-11 09:33:42 +0200

    Use updated travis.sh, most importantly export DEPLOY_PULL_REQUEST=true

--- travis.sh
+++ travis.sh
@@ -2,12 +2,12 @@
 
 set -euo pipefail
 
-function installTravisTools {
+function configureTravis {
   mkdir ~/.local
-  curl -sSL https://github.com/SonarSource/travis-utils/tarball/v31 | tar zx --strip-components 1 -C ~/.local
+  curl -sSL https://github.com/SonarSource/travis-utils/tarball/v33 | tar zx --strip-components 1 -C ~/.local
   source ~/.local/bin/install
 }
+configureTravis
 
-installTravisTools
-
+export DEPLOY_PULL_REQUEST=true
 regular_mvn_build_deploy_analyze
