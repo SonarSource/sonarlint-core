6def24f8ac2b82fd908205c7909671bd635ecab4
Janos Gyerik <janos.gyerik@sonarsource.com>
2016-10-11 09:55:54 +0200

    Update artifact file format for travis tools v33

--- cix.sh
+++ cix.sh
@@ -4,7 +4,7 @@ set -euo pipefail
 #deploy the version built by travis
 CURRENT_VERSION=`mvn help:evaluate -Dexpression="project.version" | grep -v '^\[\|Download\w\+\:'`
 RELEASE_VERSION=`echo $CURRENT_VERSION | sed "s/-.*//g"`
-NEW_VERSION="$RELEASE_VERSION-build$CI_BUILD_NUMBER"
+NEW_VERSION="$RELEASE_VERSION.$CI_BUILD_NUMBER"
 echo $NEW_VERSION
 
 mvn versions:set -DnewVersion=$NEW_VERSION
