391f2a43744e120e6e20079e3f533d1e102f930f
tomverin <thomas.verin@sonarsource.com>
2016-11-10 15:36:43 +0100

    build: build and sign on cix (#68)

---
+++ build.sh
@@ -0,0 +1,116 @@
+#!/bin/bash
+# Regular way to build a SonarSource Maven project on Travis.
+# Requires the environment variables:
+# - SONAR_HOST_URL: URL of SonarQube server
+# - SONAR_TOKEN: access token to send analysis reports to $SONAR_HOST_URL
+# - GITHUB_TOKEN: access token to send analysis of pull requests to GibHub
+# - ARTIFACTORY_URL: URL to Artifactory repository
+# - ARTIFACTORY_DEPLOY_REPO: name of deployment repository
+# - ARTIFACTORY_DEPLOY_USERNAME: login to deploy to $ARTIFACTORY_DEPLOY_REPO
+# - ARTIFACTORY_DEPLOY_PASSWORD: password to deploy to $ARTIFACTORY_DEPLOY_REPO
+
+set -euo pipefail
+
+function configureTravis {
+  mkdir -p .local
+  curl -sSL https://github.com/SonarSource/travis-utils/tarball/v33 | tar zx --strip-components 1 -C .local  
+}
+configureTravis
+
+export PATH=`pwd`/.local/bin:$PATH
+
+if [ "${TRAVIS_BRANCH}" == "master" ] && [ "$TRAVIS_PULL_REQUEST" == "false" ]; then
+  echo '======= Build, deploy and analyze master'
+
+  # Fetch all commit history so that SonarQube has exact blame information
+  # for issue auto-assignment
+  # This command can fail with "fatal: --unshallow on a complete repository does not make sense" 
+  # if there are not enough commits in the Git repository (even if Travis executed git clone --depth 50).
+  # For this reason errors are ignored with "|| true"
+  git fetch --unshallow || true
+
+  # Analyze with SNAPSHOT version as long as SQ does not correctly handle
+  # purge of release data
+  CURRENT_VERSION=`maven_expression "project.version"`
+
+  . set_maven_build_version $TRAVIS_BUILD_NUMBER
+  
+  export MAVEN_OPTS="-Xmx1536m -Xms128m"
+  mvn org.jacoco:jacoco-maven-plugin:prepare-agent deploy sonar:sonar \
+      -Pcoverage-per-test,deploy-sonarsource,release,sign \
+      -Dsonarsource.keystore.path=$SONARSOURCE_KEYSTORE_PATH \
+      -Dsonarsource.keystore.password=$SONARSOURCE_KEYSTORE_PASS \
+      -Dmaven.test.redirectTestOutputToFile=false \
+      -Dsonar.host.url=$SONAR_HOST_URL \
+      -Dsonar.login=$SONAR_TOKEN \
+      -Dsonar.projectVersion=$CURRENT_VERSION \
+      -B -e -V $*
+
+elif [[ "${TRAVIS_BRANCH}" == "branch-"* ]] && [ "$TRAVIS_PULL_REQUEST" == "false" ]; then
+  # no dory analysis on release branch
+
+  # Fetch all commit history so that SonarQube has exact blame information
+  # for issue auto-assignment
+  # This command can fail with "fatal: --unshallow on a complete repository does not make sense" 
+  # if there are not enough commits in the Git repository (even if Travis executed git clone --depth 50).
+  # For this reason errors are ignored with "|| true"
+  git fetch --unshallow || true
+
+  export MAVEN_OPTS="-Xmx1536m -Xms128m" 
+
+  # get current version from pom
+  CURRENT_VERSION=`maven_expression "project.version"`
+  
+  if [[ $CURRENT_VERSION =~ "-SNAPSHOT" ]]; then
+    echo "======= Found SNAPSHOT version ======="
+    # Do not deploy a SNAPSHOT version but the release version related to this build
+    . set_maven_build_version $TRAVIS_BUILD_NUMBER
+    mvn deploy \
+      -Pdeploy-sonarsource,release,sign \
+      -Dsonarsource.keystore.path=$SONARSOURCE_KEYSTORE_PATH \
+      -Dsonarsource.keystore.password=$SONARSOURCE_KEYSTORE_PASS \
+      -B -e -V $*
+  else
+    echo "======= Found RELEASE version ======="
+    mvn deploy \
+      -Pdeploy-sonarsource,release,sign \
+      -Dsonarsource.keystore.path=$SONARSOURCE_KEYSTORE_PATH \
+      -Dsonarsource.keystore.password=$SONARSOURCE_KEYSTORE_PASS \
+      -B -e -V $*
+  fi
+
+elif [ "$TRAVIS_PULL_REQUEST" != "false" ] && [ -n "${GITHUB_TOKEN:-}" ]; then
+  echo '======= Build and analyze pull request'
+  
+  # Do not deploy a SNAPSHOT version but the release version related to this build and PR
+  . set_maven_build_version $TRAVIS_BUILD_NUMBER
+
+  # No need for Maven phase "install" as the generated JAR files do not need to be installed
+  # in Maven local repository. Phase "verify" is enough.
+
+  export MAVEN_OPTS="-Xmx1G -Xms128m"
+  echo '======= with deploy'
+  mvn org.jacoco:jacoco-maven-plugin:prepare-agent deploy sonar:sonar \
+      -Pdeploy-sonarsource \
+      -Dmaven.test.redirectTestOutputToFile=false \
+      -Dsonar.analysis.mode=issues \
+      -Dsonar.github.pullRequest=$TRAVIS_PULL_REQUEST \
+      -Dsonar.github.repository=$TRAVIS_REPO_SLUG \
+      -Dsonar.github.oauth=$GITHUB_TOKEN \
+      -Dsonar.host.url=$SONAR_HOST_URL \
+      -Dsonar.login=$SONAR_TOKEN \
+      -B -e -V $*
+  
+else
+  echo '======= Build, no analysis, no deploy'
+
+  # No need for Maven phase "install" as the generated JAR files do not need to be installed
+  # in Maven local repository. Phase "verify" is enough.
+
+  mvn verify \
+      -Dmaven.test.redirectTestOutputToFile=false \
+      -B -e -V $*
+
+  #in order to stop QA from starting, the build.properties file is removed
+  rm build.properties
+fi
