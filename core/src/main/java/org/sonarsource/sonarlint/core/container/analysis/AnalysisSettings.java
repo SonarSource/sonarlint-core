12aea824a07128580195e03db57baa1b95ca6e91
Duarte Meneses <duarte.meneses@sonarsource.com>
2016-12-01 13:35:43 +0100

    SLCORE-84 Disable cpp by default

--- core/src/main/java/org/sonarsource/sonarlint/core/container/analysis/AnalysisSettings.java
+++ core/src/main/java/org/sonarsource/sonarlint/core/container/analysis/AnalysisSettings.java
@@ -48,12 +48,12 @@ public class AnalysisSettings extends Settings {
   public AnalysisSettings(StorageManager storage, StandaloneAnalysisConfiguration config, PropertyDefinitions propertyDefinitions) {
     super(propertyDefinitions, new Encryption(null));
     GlobalProperties globalProps = storage.readGlobalPropertiesFromStorage();
-    addDefaultProperties();
     addProperties(globalProps.getPropertiesMap());
     if (config instanceof ConnectedAnalysisConfiguration && ((ConnectedAnalysisConfiguration) config).moduleKey() != null) {
       ModuleConfiguration projectConfig = storage.readModuleConfigFromStorage(((ConnectedAnalysisConfiguration) config).moduleKey());
       addProperties(projectConfig.getPropertiesMap());
     }
+    addDefaultProperties();
     addProperties(config.extraProperties());
   }
   
