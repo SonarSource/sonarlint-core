5413bcf936151f339ae49a83c9591f23740c533a
Julien HENRY <julien.henry@sonarsource.com>
2017-01-24 17:29:37 +0100

    SLCORE-94 Use pagination with WS api/components/tree

--- core/src/test/java/org/sonarsource/sonarlint/core/container/connected/update/ModuleHierarchyDownloaderTest.java
+++ core/src/test/java/org/sonarsource/sonarlint/core/container/connected/update/ModuleHierarchyDownloaderTest.java
@@ -19,18 +19,22 @@
  */
 package org.sonarsource.sonarlint.core.container.connected.update;
 
-import static org.assertj.core.api.Assertions.assertThat;
-import static org.assertj.core.api.Assertions.entry;
-import static org.mockito.Mockito.mock;
-
+import java.io.IOException;
 import java.util.Map;
-
 import org.junit.Before;
 import org.junit.Rule;
 import org.junit.Test;
 import org.junit.rules.ExpectedException;
+import org.sonarqube.ws.Common.Paging;
+import org.sonarqube.ws.WsComponents.Component;
+import org.sonarqube.ws.WsComponents.TreeWsResponse;
 import org.sonarsource.sonarlint.core.WsClientTestUtils;
 import org.sonarsource.sonarlint.core.container.connected.SonarLintWsClient;
+
+import static org.assertj.core.api.Assertions.assertThat;
+import static org.assertj.core.api.Assertions.entry;
+import static org.mockito.Mockito.mock;
+import static org.sonarsource.sonarlint.core.container.connected.update.ModuleHierarchyDownloader.PAGE_SIZE;
 
 public class ModuleHierarchyDownloaderTest {
   private SonarLintWsClient wsClient;
@@ -42,8 +46,14 @@ public class ModuleHierarchyDownloaderTest {
   @Before
   public void setUp() {
     wsClient = mock(SonarLintWsClient.class);
+
+    downloader = new ModuleHierarchyDownloader(wsClient);
+  }
+
+  @Test
+  public void simpleTest() {
     WsClientTestUtils
-      .addStreamResponse(wsClient, "api/components/tree.protobuf?qualifiers=TRK,BRC&baseComponentKey=testRoot",
+      .addStreamResponse(wsClient, "api/components/tree.protobuf?ps=500&p=1&qualifiers=BRC&baseComponentKey=testRoot",
         "/update/tree.pb");
     WsClientTestUtils.addStreamResponse(wsClient, "api/components/show.protobuf?id=AVeumyM5SDj1uJJMtck2",
       "/update/show_module1.pb");
@@ -53,12 +63,6 @@ public class ModuleHierarchyDownloaderTest {
       "/update/show_module1_module12.pb");
     WsClientTestUtils.addStreamResponse(wsClient, "api/components/show.protobuf?id=AVeumyM5SDj1uJJMtck9",
       "/update/show_module2.pb");
-
-    downloader = new ModuleHierarchyDownloader(wsClient);
-  }
-
-  @Test
-  public void test() {
     Map<String, String> fetchModuleHierarchy = downloader.fetchModuleHierarchy("testRoot");
     assertThat(fetchModuleHierarchy).contains(
       entry("testRoot", ""),
@@ -69,9 +73,56 @@ public class ModuleHierarchyDownloaderTest {
   }
 
   @Test
+  public void testNoPaginationWhenJustUnderPageSize() throws IOException {
+    TreeWsResponse.Builder responseBuilder = TreeWsResponse.newBuilder()
+      .setPaging(Paging.newBuilder().setTotal(PAGE_SIZE));
+    for (int i = 0; i < PAGE_SIZE; i++) {
+      responseBuilder.addComponents(Component.newBuilder()
+        .setId("id" + i)
+        .setKey("testRoot" + i));
+      WsClientTestUtils.addStreamResponse(wsClient, "api/components/show.protobuf?id=id" + i, "/update/show_module1.pb");
+    }
+
+    WsClientTestUtils
+      .addResponse(wsClient, "api/components/tree.protobuf?ps=500&p=1&qualifiers=BRC&baseComponentKey=testRoot", responseBuilder.build());
+
+    Map<String, String> fetchModuleHierarchy = downloader.fetchModuleHierarchy("testRoot");
+    assertThat(fetchModuleHierarchy).hasSize(PAGE_SIZE + 1 /* root module */);
+  }
+
+  @Test
+  public void testPagination() throws IOException {
+    TreeWsResponse.Builder responseBuilder = TreeWsResponse.newBuilder()
+      .setPaging(Paging.newBuilder().setPageIndex(1).setTotal(501));
+    for (int i = 0; i < PAGE_SIZE; i++) {
+      responseBuilder.addComponents(Component.newBuilder()
+        .setId("id" + i)
+        .setKey("testRoot" + i));
+      WsClientTestUtils.addStreamResponse(wsClient, "api/components/show.protobuf?id=id" + i, "/update/show_module1.pb");
+    }
+
+    WsClientTestUtils
+      .addResponse(wsClient, "api/components/tree.protobuf?ps=500&p=1&qualifiers=BRC&baseComponentKey=testRoot", responseBuilder.build());
+
+    responseBuilder = TreeWsResponse.newBuilder()
+      .setPaging(Paging.newBuilder().setPageIndex(2).setTotal(501));
+    for (int i = 501; i < 502; i++) {
+      responseBuilder.addComponents(Component.newBuilder()
+        .setId("id" + i)
+        .setKey("testRoot" + i));
+      WsClientTestUtils.addStreamResponse(wsClient, "api/components/show.protobuf?id=id" + i, "/update/show_module1.pb");
+    }
+    WsClientTestUtils
+      .addResponse(wsClient, "api/components/tree.protobuf?ps=500&p=2&qualifiers=BRC&baseComponentKey=testRoot", responseBuilder.build());
+
+    Map<String, String> fetchModuleHierarchy = downloader.fetchModuleHierarchy("testRoot");
+    assertThat(fetchModuleHierarchy).hasSize(501 + 1);
+  }
+
+  @Test
   public void testIOException() {
     wsClient = mock(SonarLintWsClient.class);
-    WsClientTestUtils.addFailedResponse(wsClient, "api/components/tree.protobuf?qualifiers=TRK,BRC&baseComponentKey=testRoot", 503, "error");
+    WsClientTestUtils.addFailedResponse(wsClient, "api/components/tree.protobuf?ps=500&p=1&qualifiers=BRC&baseComponentKey=testRoot", 503, "error");
     downloader = new ModuleHierarchyDownloader(wsClient);
     exception.expect(IllegalStateException.class);
     exception.expectMessage("Error 503");
@@ -82,7 +133,7 @@ public class ModuleHierarchyDownloaderTest {
   public void testInvalidResponseContent() {
     wsClient = mock(SonarLintWsClient.class);
     WsClientTestUtils
-      .addResponse(wsClient, "api/components/tree.protobuf?qualifiers=TRK,BRC&baseComponentKey=testRoot", "invalid response stream");
+      .addResponse(wsClient, "api/components/tree.protobuf?ps=500&p=1&qualifiers=BRC&baseComponentKey=testRoot", "invalid response stream");
     downloader = new ModuleHierarchyDownloader(wsClient);
     exception.expect(IllegalStateException.class);
     exception.expectMessage("Failed to load module hierarchy");
