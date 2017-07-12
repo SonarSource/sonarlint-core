348c795b1d1b03fd8a1204d129b420eda39c40f7
Julien HENRY <henryju@yahoo.fr>
2017-07-12 18:33:00 +0200

    SLCORE-140 Return issue type in daemon protocol

--- daemon/src/test/java/org/sonarlint/daemon/model/ProxyIssueListenerTest.java
+++ daemon/src/test/java/org/sonarlint/daemon/model/ProxyIssueListenerTest.java
@@ -19,18 +19,18 @@
  */
 package org.sonarlint.daemon.model;
 
-import static org.assertj.core.api.Assertions.assertThat;
-import static org.mockito.Mockito.mock;
-import static org.mockito.Mockito.verify;
-import static org.mockito.Mockito.when;
-
+import io.grpc.stub.StreamObserver;
 import org.junit.Test;
 import org.mockito.ArgumentCaptor;
 import org.sonarsource.sonarlint.core.client.api.common.analysis.ClientInputFile;
 import org.sonarsource.sonarlint.daemon.proto.SonarlintDaemon.Issue;
 import org.sonarsource.sonarlint.daemon.proto.SonarlintDaemon.Issue.Severity;
+import org.sonarsource.sonarlint.daemon.proto.SonarlintDaemon.Issue.Type;
 
-import io.grpc.stub.StreamObserver;
+import static org.assertj.core.api.Assertions.assertThat;
+import static org.mockito.Mockito.mock;
+import static org.mockito.Mockito.verify;
+import static org.mockito.Mockito.when;
 
 public class ProxyIssueListenerTest {
   @Test
@@ -40,6 +40,13 @@ public class ProxyIssueListenerTest {
         continue;
       }
       testWithSeverity(s);
+    }
+
+    for (Type t : Type.values()) {
+      if (t == Type.UNRECOGNIZED) {
+        continue;
+      }
+      testWithType(t);
     }
   }
 
@@ -75,6 +82,43 @@ public class ProxyIssueListenerTest {
     assertThat(captured.getRuleKey()).isEqualTo("key");
     assertThat(captured.getRuleName()).isEqualTo("name");
     assertThat(captured.getSeverity()).isEqualTo(severity);
+    assertThat(captured.getFilePath()).isEqualTo("filename");
+    assertThat(captured.getUserObject()).isEqualTo("obj");
+  }
+
+  private void testWithType(Type type) {
+    StreamObserver<Issue> observer = mock(StreamObserver.class);
+    ClientInputFile inputFile = mock(ClientInputFile.class);
+    when(inputFile.getPath()).thenReturn("filename");
+    when(inputFile.getClientObject()).thenReturn("obj");
+    ProxyIssueListener listener = new ProxyIssueListener(observer);
+
+    org.sonarsource.sonarlint.core.client.api.common.analysis.Issue i = mock(org.sonarsource.sonarlint.core.client.api.common.analysis.Issue.class);
+    when(i.getEndLine()).thenReturn(10);
+    when(i.getStartLine()).thenReturn(11);
+    when(i.getStartLineOffset()).thenReturn(12);
+    when(i.getEndLineOffset()).thenReturn(13);
+    when(i.getMessage()).thenReturn("msg");
+    when(i.getRuleKey()).thenReturn("key");
+    when(i.getRuleName()).thenReturn("name");
+    when(i.getSeverity()).thenReturn("MAJOR");
+    when(i.getType()).thenReturn(type.toString().toUpperCase());
+    when(i.getInputFile()).thenReturn(inputFile);
+
+    listener.handle(i);
+
+    ArgumentCaptor<Issue> argument = ArgumentCaptor.forClass(Issue.class);
+    verify(observer).onNext(argument.capture());
+
+    Issue captured = argument.getValue();
+    assertThat(captured.getEndLine()).isEqualTo(10);
+    assertThat(captured.getStartLine()).isEqualTo(11);
+    assertThat(captured.getStartLineOffset()).isEqualTo(12);
+    assertThat(captured.getEndLineOffset()).isEqualTo(13);
+    assertThat(captured.getMessage()).isEqualTo("msg");
+    assertThat(captured.getRuleKey()).isEqualTo("key");
+    assertThat(captured.getRuleName()).isEqualTo("name");
+    assertThat(captured.getType()).isEqualTo(type);
     assertThat(captured.getFilePath()).isEqualTo("filename");
     assertThat(captured.getUserObject()).isEqualTo("obj");
   }
