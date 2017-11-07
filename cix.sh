13c14f479de66725c38ebb37b43fcb96e06533fa
Duarte Meneses <duarte.meneses@sonarsource.com>
2017-11-07 10:39:38 +0100

    Fix ITs

--- cix.sh
+++ cix.sh
@@ -4,53 +4,5 @@ set -euo pipefail
 plugins_min_versions_path=core/src/main/resources/plugins_min_versions.txt
 
 ./set_maven_build_version.sh "$CI_BUILD_NUMBER"
+./run-integration-tests.sh "$SQ_VERSION"
 
-cd its
-
-case "$SQ_VERSION" in
-  LTS)
-    minVersions=$(sed -ne '/^[a-z]*=[0-9.]*$/s/$/;/p' < "../$plugins_min_versions_path")
-    eval "$minVersions"
-    JAVA_VERSION=$java
-    PHP_VERSION=$php
-    JAVASCRIPT_VERSION=$javascript
-    JAVASCRIPT_BUILD_VERSION=2.14
-    PYTHON_VERSION=$python
-    COBOL_VERSION=$cobol
-    ;;
-  LATEST_RELEASE)
-    JAVA_VERSION=LATEST_RELEASE
-    PHP_VERSION=LATEST_RELEASE
-    JAVASCRIPT_VERSION=LATEST_RELEASE
-    # There was an API change that requires to build with this version
-    JAVASCRIPT_BUILD_VERSION=2.20.0.4207
-    PYTHON_VERSION=LATEST_RELEASE
-    # use old license
-    COBOL_VERSION=4.0.1.2609
-    ;;
-  DEV)
-    JAVA_VERSION=LATEST_RELEASE
-    PHP_VERSION=LATEST_RELEASE
-    JAVASCRIPT_VERSION=LATEST_RELEASE
-    # There was an API change that requires to build with this version
-    JAVASCRIPT_BUILD_VERSION=2.20.0.4207
-    PYTHON_VERSION=LATEST_RELEASE
-    # use new license mechanism
-    COBOL_VERSION=LATEST_RELEASE
-    ;; 
-  *)
-    echo "fatal: unknown SQ_VERSION value '$SQ_VERSION'"
-    exit 1
-esac
-
-echo "Running with SQ=$SQ_VERSION JAVA_VERSION=$JAVA_VERSION JAVASCRIPT_VERSION=$JAVASCRIPT_VERSION PHP_VERSION=$PHP_VERSION PYTHON_VERSION=$PYTHON_VERSION COBOL_VERSION=$COBOL_VERSION"
-
-export MAVEN_OPTS=-Xmx1024m
-
-mvn verify -Pits -Dsonar.runtimeVersion=$SQ_VERSION \
-    -DjavaVersion=$JAVA_VERSION \
-    -DphpVersion=$PHP_VERSION \
-    -DjavascriptVersion=$JAVASCRIPT_VERSION \
-    -Djavascript.buildVersion=$JAVASCRIPT_BUILD_VERSION \
-    -DpythonVersion=$PYTHON_VERSION \
-    -DcobolVersion=$COBOL_VERSION
