4222e4785686f870167c6ed6dc03480bc8f8f7f6
Janos Gyerik <janos.gyerik@sonarsource.com>
2017-11-07 11:09:09 +0100

    Move plugins_min_versions_path variable where needed

--- cix.sh
+++ cix.sh
@@ -1,8 +1,6 @@
 #!/bin/bash
 set -euo pipefail
 
-plugins_min_versions_path=core/src/main/resources/plugins_min_versions.txt
-
 ./set_maven_build_version.sh "$CI_BUILD_NUMBER"
 ./run-integration-tests.sh "$SQ_VERSION"
 
