9dfd952d4946031d26dba52eeead3a45380bb7a9
Julien HENRY <julien.henry@sonarsource.com>
2018-01-26 11:27:01 +0100

    Fix license headers of ITs

--- its/plugins/global-extension-plugin/src/main/java/org/sonarsource/plugins/example/GlobalSensor.java
+++ its/plugins/global-extension-plugin/src/main/java/org/sonarsource/plugins/example/GlobalSensor.java
@@ -1,6 +1,6 @@
 /*
  * Example Plugin with global extension
- * Copyright (C) 2009-2017 SonarSource SA
+ * Copyright (C) 2009-2018 SonarSource SA
  * mailto:info AT sonarsource DOT com
  *
  * This program is free software; you can redistribute it and/or
