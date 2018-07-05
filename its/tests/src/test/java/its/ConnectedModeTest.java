2704a64ca90741d53521ca8939efbbe9d4a63a67
Janos Gyerik <janos.gyerik@sonarsource.com>
2018-07-05 15:26:50 +0200

    Use latest Python for 6.7+ only

--- its/tests/src/test/java/its/ConnectedModeTest.java
+++ its/tests/src/test/java/its/ConnectedModeTest.java
@@ -114,16 +114,17 @@ public class ConnectedModeTest extends AbstractConnectedTest {
 
       if (atLeast67) {
         orchestratorBuilder
-          .addPlugin(MavenLocation.of("org.sonarsource.java", "sonar-java-plugin", "LATEST_RELEASE"));
+          .addPlugin(MavenLocation.of("org.sonarsource.java", "sonar-java-plugin", "LATEST_RELEASE"))
+          .addPlugin(MavenLocation.of("org.sonarsource.python", "sonar-python-plugin", "LATEST_RELEASE"));
       } else {
         orchestratorBuilder
-          .addPlugin(MavenLocation.of("org.sonarsource.java", "sonar-java-plugin", "4.15.0.12310"));
+          .addPlugin(MavenLocation.of("org.sonarsource.java", "sonar-java-plugin", "4.15.0.12310"))
+          .addPlugin(MavenLocation.of("org.sonarsource.python", "sonar-python-plugin", "1.8.0.1496"));
       }
 
       orchestratorBuilder
         .addPlugin(MavenLocation.of("org.sonarsource.javascript", "sonar-javascript-plugin", "LATEST_RELEASE"))
         .addPlugin(MavenLocation.of("org.sonarsource.php", "sonar-php-plugin", "LATEST_RELEASE"))
-        .addPlugin(MavenLocation.of("org.sonarsource.python", "sonar-python-plugin", "LATEST_RELEASE"))
         .addPlugin(MavenLocation.of("org.sonarsource.web", "sonar-web-plugin", "LATEST_RELEASE"))
         .addPlugin(FileLocation.of("../plugins/javascript-custom-rules/target/javascript-custom-rules-plugin.jar"))
         .addPlugin(FileLocation.of("../plugins/custom-sensor-plugin/target/custom-sensor-plugin.jar"))
