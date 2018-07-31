0dbe64f563d29bb00dd951d60fb715b0695eaa17
Pierre-Yves Nicolas <pierre-yves.nicolas@sonarsource.com>
2018-07-31 17:14:52 +0200

    SONARWEB-72 Add SonarHTML to whitelist

--- core/src/main/java/org/sonarsource/sonarlint/core/plugin/PluginCacheLoader.java
+++ core/src/main/java/org/sonarsource/sonarlint/core/plugin/PluginCacheLoader.java
@@ -38,7 +38,7 @@ import org.sonarsource.sonarlint.core.plugin.cache.PluginCache;
 public class PluginCacheLoader {
 
   private static final ImmutableSet<String> PLUGIN_WHITELIST = ImmutableSet.of("xoo", "java", "javascript", "php", "python", "cobol", "abap", "plsql", "swift",
-    "rpg", "cpp", "pli", "typescript", "web");
+    "rpg", "cpp", "pli", "typescript", "web", "html");
   private static final String IMPLEMENTED_SQ_API = "7.1";
 
   private static final Logger LOG = Loggers.get(PluginCacheLoader.class);
