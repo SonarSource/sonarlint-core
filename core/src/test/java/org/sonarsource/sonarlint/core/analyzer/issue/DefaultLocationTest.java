715bc33e50ae60bd22f9dabb54c38f0518d2a9d8
Janos Gyerik <janos.gyerik@sonarsource.com>
2018-08-14 13:14:55 +0200

    Add DefaultLocationTest

---
+++ core/src/test/java/org/sonarsource/sonarlint/core/analyzer/issue/DefaultLocationTest.java
@@ -0,0 +1,38 @@
+/*
+ * SonarLint Core - Implementation
+ * Copyright (C) 2009-2018 SonarSource SA
+ * mailto:info AT sonarsource DOT com
+ *
+ * This program is free software; you can redistribute it and/or
+ * modify it under the terms of the GNU Lesser General Public
+ * License as published by the Free Software Foundation; either
+ * version 3 of the License, or (at your option) any later version.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the GNU
+ * Lesser General Public License for more details.
+ *
+ * You should have received a copy of the GNU Lesser General Public License
+ * along with this program; if not, write to the Free Software Foundation,
+ * Inc., 51 Franklin Street, Fifth Floor, Boston, MA  02110-1301, USA.
+ */
+package org.sonarsource.sonarlint.core.analyzer.issue;
+
+import org.junit.*;
+import org.sonarsource.sonarlint.core.client.api.common.analysis.ClientInputFile;
+
+import static org.assertj.core.api.Assertions.assertThat;
+import static org.mockito.Mockito.*;
+
+public class DefaultLocationTest {
+  @Test
+  public void verify_accessors() {
+    ClientInputFile inputFile = mock(ClientInputFile.class);
+    String message = "fummy";
+    DefaultLocation defaultLocation = new DefaultLocation(inputFile, null, message);
+
+    assertThat(defaultLocation.getInputFile()).isSameAs(inputFile);
+    assertThat(defaultLocation.getMessage()).isSameAs(message);
+  }
+}
