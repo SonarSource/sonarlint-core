14462bb20769fcf7110dcbb3a774fe433d37fcb5
Julien HENRY <julien.henry@sonarsource.com>
2019-07-02 18:10:24 +0200

    SLCORE-229 Update plugin-api to 7.9

--- core/src/main/java/org/sonarsource/sonarlint/core/container/standalone/StandaloneGlobalContainer.java
+++ core/src/main/java/org/sonarsource/sonarlint/core/container/standalone/StandaloneGlobalContainer.java
@@ -26,7 +26,7 @@ import javax.annotation.CheckForNull;
 import org.sonar.api.Plugin;
 import org.sonar.api.SonarQubeVersion;
 import org.sonar.api.batch.rule.Rules;
-import org.sonar.api.internal.ApiVersion;
+import org.sonar.api.internal.MetadataLoader;
 import org.sonar.api.internal.SonarRuntimeImpl;
 import org.sonar.api.utils.System2;
 import org.sonar.api.utils.UriReader;
@@ -74,7 +74,7 @@ public class StandaloneGlobalContainer extends ComponentContainer {
 
   @Override
   protected void doBeforeStart() {
-    Version version = ApiVersion.load(System2.INSTANCE);
+    Version version = MetadataLoader.loadVersion(System2.INSTANCE);
     add(
       StandalonePluginIndex.class,
       PluginRepository.class,
--- core/src/main/java/org/sonarsource/sonarlint/core/plugin/PluginCacheLoader.java
+++ core/src/main/java/org/sonarsource/sonarlint/core/plugin/PluginCacheLoader.java
@@ -36,7 +36,7 @@ import org.sonarsource.sonarlint.core.plugin.cache.PluginCache;
 
 public class PluginCacheLoader {
 
-  private static final String IMPLEMENTED_SQ_API = "7.6";
+  private static final String IMPLEMENTED_SQ_API = "7.9";
 
   private static final Logger LOG = Loggers.get(PluginCacheLoader.class);
 
