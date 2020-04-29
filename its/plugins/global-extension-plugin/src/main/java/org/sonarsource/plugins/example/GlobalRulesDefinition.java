0480cb32a7b530d9833e70e368e4698b283b07a5
Jean-Baptiste Lievremont <jeanbaptiste.lievremont@sonarsource.com>
2020-04-29 19:45:37 +0200

    SLCORE-269 Add parameters with all supported types to rule of plugin used in ITs

--- its/plugins/global-extension-plugin/src/main/java/org/sonarsource/plugins/example/GlobalRulesDefinition.java
+++ its/plugins/global-extension-plugin/src/main/java/org/sonarsource/plugins/example/GlobalRulesDefinition.java
@@ -19,6 +19,7 @@
  */
 package org.sonarsource.plugins.example;
 
+import org.sonar.api.server.rule.RuleParamType;
 import org.sonar.api.server.rule.RulesDefinition;
 
 public final class GlobalRulesDefinition implements RulesDefinition {
@@ -30,10 +31,38 @@ public final class GlobalRulesDefinition implements RulesDefinition {
   @Override
   public void define(Context context) {
     NewRepository repository = context.createRepository(KEY, GlobalLanguage.LANGUAGE_KEY).setName(NAME);
-    repository.createRule(RULE_KEY)
+    NewRule rule = repository.createRule(RULE_KEY)
       .setActivatedByDefault(true)
       .setName("Increment")
       .setHtmlDescription("Increment message after every analysis");
+    rule.createParam("stringParam")
+      .setType(RuleParamType.STRING)
+      .setDefaultValue("string parameter")
+      .setDescription("A string parameter");
+    rule.createParam("textParam")
+      .setType(RuleParamType.TEXT)
+      .setDefaultValue("text\nparameter")
+      .setDescription("A text parameter");
+    rule.createParam("intParam")
+      .setType(RuleParamType.INTEGER)
+      .setDefaultValue("42")
+      .setDescription("An int parameter");
+    rule.createParam("boolParam")
+      .setType(RuleParamType.BOOLEAN)
+      .setDefaultValue("true")
+      .setDescription("A boolean parameter");
+    rule.createParam("floatParam")
+      .setType(RuleParamType.FLOAT)
+      .setDefaultValue("3.14159265358")
+      .setDescription("A float parameter");
+    rule.createParam("enumParam")
+      .setType(RuleParamType.singleListOfValues("enum1", "enum2", "enum3"))
+      .setDefaultValue("enum1")
+      .setDescription("An enum parameter");
+    rule.createParam("enumListParam")
+      .setType(RuleParamType.multipleListOfValues("list1", "list2", "list3"))
+      .setDefaultValue("list1,list2")
+      .setDescription("An enum list parameter");
 
     repository.done();
   }
