6b7dbd0dd8f338272357d43c089f04061197ac96
Julien HENRY <julien.henry@sonarsource.com>
2020-11-09 18:00:50 +0100

    SLCORE-282 Add telemetry field 'dev_notifications_received'

--- core/src/test/java/org/sonarsource/sonarlint/core/telemetry/TelemetryPayloadTest.java
+++ core/src/test/java/org/sonarsource/sonarlint/core/telemetry/TelemetryPayloadTest.java
@@ -39,7 +39,7 @@ public class TelemetryPayloadTest {
     distrib.put("1000-2000", BigDecimal.valueOf(90.10));
     perf[0] = new TelemetryAnalyzerPerformancePayload("java", distrib);
     TelemetryPayload m = new TelemetryPayload(4, 15, "SLI", "2.4", "Pycharm 3.2",
-      true, true, systemTime, installTime, "Windows 10", "1.8.0", "10.5.2", true, perf);
+      true, true, systemTime, installTime, "Windows 10", "1.8.0", "10.5.2", true, 3, perf);
     String s = m.toJson();
 
     assertThat(s).isEqualTo("{\"days_since_installation\":4,"
@@ -55,7 +55,8 @@ public class TelemetryPayloadTest {
       + "\"jre\":\"1.8.0\","
       + "\"nodejs\":\"10.5.2\","
       + "\"analyses\":[{\"language\":\"java\",\"rate_per_duration\":{\"0-300\":9.9,\"1000-2000\":90.1}}],"
-      + "\"dev_notifications_disabled\":true}");
+      + "\"dev_notifications_disabled\":true,"
+      + "\"dev_notifications_received\":3}");
 
     assertThat(m.daysOfUse()).isEqualTo(15);
     assertThat(m.daysSinceInstallation()).isEqualTo(4);
@@ -69,5 +70,6 @@ public class TelemetryPayloadTest {
     assertThat(m.connectedModeSonarcloud()).isTrue();
     assertThat(m.systemTime()).isEqualTo(systemTime);
     assertThat(m.devNotificationsDisabled()).isTrue();
+    assertThat(m.devNotificationsReceived()).isEqualTo(3);
   }
 }
