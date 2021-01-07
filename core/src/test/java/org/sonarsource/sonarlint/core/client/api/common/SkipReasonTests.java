102c96b24aab75d4c97a3a38e5d5c5ef0bcbfad9
Julien HENRY <julien.henry@sonarsource.com>
2021-01-07 13:52:59 +0100

    Fix quality flaws

--- core/src/test/java/org/sonarsource/sonarlint/core/client/api/common/SkipReasonTests.java
+++ core/src/test/java/org/sonarsource/sonarlint/core/client/api/common/SkipReasonTests.java
@@ -33,64 +33,68 @@ import static org.assertj.core.api.Assertions.assertThat;
 class SkipReasonTests {
 
   @Test
-  void testLanguageNotEnabled() {
+  void testLanguageNotEnabled_getters_equals_hashcode_tostring() {
     SkipReason.LanguagesNotEnabled underTest = new LanguagesNotEnabled(asList(Language.JAVA));
     // Getters
-    assertThat(underTest.getNotEnabledLanguages()).containsExactly(Language.JAVA);
-    // Equals
-    assertThat(underTest).isEqualTo(underTest);
-    assertThat(underTest).isNotEqualTo(IncompatiblePluginApi.INSTANCE);
-    assertThat(underTest).isNotEqualTo(new LanguagesNotEnabled(asList(Language.JS)));
-    assertThat(underTest).isEqualTo(new LanguagesNotEnabled(asList(Language.JAVA)));
-    // HashCode
-    assertThat(underTest).hasSameHashCodeAs(underTest);
-    assertThat(underTest).hasSameHashCodeAs(new LanguagesNotEnabled(asList(Language.JAVA)));
-    // To String
-    assertThat(underTest.toString()).isEqualTo("LanguagesNotEnabled [languages=[Java]]");
+    assertThat(underTest.getNotEnabledLanguages())
+      .containsExactly(Language.JAVA);
+    assertThat(underTest)
+      // Equals
+      .isEqualTo(underTest)
+      .isNotEqualTo(IncompatiblePluginApi.INSTANCE)
+      .isNotEqualTo(new LanguagesNotEnabled(asList(Language.JS)))
+      .isEqualTo(new LanguagesNotEnabled(asList(Language.JAVA)))
+      // HashCode
+      .hasSameHashCodeAs(underTest)
+      .hasSameHashCodeAs(new LanguagesNotEnabled(asList(Language.JAVA)))
+      // To String
+      .hasToString("LanguagesNotEnabled [languages=[Java]]");
   }
 
   @Test
-  void testUnsatisfiedDependency() {
+  void testUnsatisfiedDependency_getters_equals_hashcode_tostring() {
     SkipReason.UnsatisfiedDependency underTest = new UnsatisfiedDependency("foo");
     // Getters
     assertThat(underTest.getDependencyKey()).isEqualTo("foo");
-    // Equals
-    assertThat(underTest).isEqualTo(underTest);
-    assertThat(underTest).isNotEqualTo(IncompatiblePluginApi.INSTANCE);
-    assertThat(underTest).isNotEqualTo(new UnsatisfiedDependency("bar"));
-    assertThat(underTest).isEqualTo(new UnsatisfiedDependency("foo"));
-    // HashCode
-    assertThat(underTest).hasSameHashCodeAs(underTest);
-    assertThat(underTest).hasSameHashCodeAs(new UnsatisfiedDependency("foo"));
-    // To String
-    assertThat(underTest.toString()).isEqualTo("UnsatisfiedDependency [dependencyKey=foo]");
+    assertThat(underTest)
+      // Equals
+      .isEqualTo(underTest)
+      .isNotEqualTo(IncompatiblePluginApi.INSTANCE)
+      .isNotEqualTo(new UnsatisfiedDependency("bar"))
+      .isEqualTo(new UnsatisfiedDependency("foo"))
+      // HashCode
+      .hasSameHashCodeAs(underTest)
+      .hasSameHashCodeAs(new UnsatisfiedDependency("foo"))
+      // To String
+      .hasToString("UnsatisfiedDependency [dependencyKey=foo]");
   }
 
   @Test
-  void testIncompatiblePluginVersion() {
+  void testIncompatiblePluginVersion_getters_equals_hashcode_tostring() {
     SkipReason.IncompatiblePluginVersion underTest = new IncompatiblePluginVersion("1.0");
     // Getters
     assertThat(underTest.getMinVersion()).isEqualTo("1.0");
-    // Equals
-    assertThat(underTest).isEqualTo(underTest);
-    assertThat(underTest).isNotEqualTo(IncompatiblePluginApi.INSTANCE);
-    assertThat(underTest).isNotEqualTo(new IncompatiblePluginVersion("2.0"));
-    assertThat(underTest).isEqualTo(new IncompatiblePluginVersion("1.0"));
-    // HashCode
-    assertThat(underTest).hasSameHashCodeAs(underTest);
-    assertThat(underTest).hasSameHashCodeAs(new IncompatiblePluginVersion("1.0"));
-    // To String
-    assertThat(underTest.toString()).isEqualTo("IncompatiblePluginVersion [minVersion=1.0]");
+    assertThat(underTest)
+      // Equals
+      .isEqualTo(underTest)
+      .isNotEqualTo(IncompatiblePluginApi.INSTANCE)
+      .isNotEqualTo(new IncompatiblePluginVersion("2.0"))
+      .isEqualTo(new IncompatiblePluginVersion("1.0"))
+      // HashCode
+      .hasSameHashCodeAs(underTest)
+      .hasSameHashCodeAs(new IncompatiblePluginVersion("1.0"))
+      // To String
+      .hasToString("IncompatiblePluginVersion [minVersion=1.0]");
   }
 
   @Test
-  void testUnsatisfiedRuntimeRequirementEqualsHashCode() {
+  void testUnsatisfiedRuntimeRequirement_getters_equals_hashcode_tostring() {
     SkipReason.UnsatisfiedRuntimeRequirement underTest = new UnsatisfiedRuntimeRequirement(RuntimeRequirement.JRE, "1.0", "2.0");
     // Getters
     assertThat(underTest.getMinVersion()).isEqualTo("2.0");
     assertThat(underTest.getCurrentVersion()).isEqualTo("1.0");
-    // Equals
     assertThat(underTest)
+      // Equals
       .isEqualTo(underTest)
       .isNotEqualTo(IncompatiblePluginApi.INSTANCE)
       .isNotEqualTo(new UnsatisfiedRuntimeRequirement(RuntimeRequirement.NODEJS, "1.0", "2.0"))
@@ -99,9 +103,9 @@ class SkipReasonTests {
       .isEqualTo(new UnsatisfiedRuntimeRequirement(RuntimeRequirement.JRE, "1.0", "2.0"))
       // HashCode
       .hasSameHashCodeAs(underTest)
-      .hasSameHashCodeAs(new UnsatisfiedRuntimeRequirement(RuntimeRequirement.JRE, "1.0", "2.0"));
-    // To String
-    assertThat(underTest.toString()).isEqualTo("UnsatisfiedRuntimeRequirement [runtime=JRE, currentVersion=1.0, minVersion=2.0]");
+      .hasSameHashCodeAs(new UnsatisfiedRuntimeRequirement(RuntimeRequirement.JRE, "1.0", "2.0"))
+      // To String
+      .hasToString("UnsatisfiedRuntimeRequirement [runtime=JRE, currentVersion=1.0, minVersion=2.0]");
   }
 
 }
