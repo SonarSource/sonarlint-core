612c500489cffb8bc789f0bd3d6144fb310f6367
Julien HENRY <julien.henry@sonarsource.com>
2021-01-07 12:30:38 +0100

    Update license headers

--- its/tests/src/test/java/its/ConnectedFileMatchingTest.java
+++ its/tests/src/test/java/its/ConnectedFileMatchingTest.java
@@ -1,6 +1,6 @@
 /*
  * SonarLint Core - ITs - Tests
- * Copyright (C) 2016-2020 SonarSource SA
+ * Copyright (C) 2016-2021 SonarSource SA
  * mailto:info AT sonarsource DOT com
  *
  * This program is free software; you can redistribute it and/or
