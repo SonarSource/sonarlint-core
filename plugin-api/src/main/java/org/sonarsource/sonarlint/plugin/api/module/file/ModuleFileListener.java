4d3da5416d08a55e2da7f9808e04120ab0b233dc
Damien Urruty <damien.urruty@sonarsource.com>
2021-05-04 17:05:31 +0200

    SLCORE-312 Introduce a SonarLint Plugin API

---
+++ plugin-api/src/main/java/org/sonarsource/sonarlint/plugin/api/module/file/ModuleFileListener.java
@@ -0,0 +1,30 @@
+/*
+ * SonarLint Plugin API
+ * Copyright (C) 2016-2021 SonarSource SA
+ * mailto:info AT sonarsource DOT com
+ *
+ * This program is free software; you can redistribute it and/or
+ * modify it under the terms of the GNU Lesser General Public
+ * License as published by the Free Software Foundation; either
+ * version 3 of the License, or (at your option) any later version.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the GNU
+ * Lesser General Public License for more details.
+ *
+ * You should have received a copy of the GNU Lesser General Public License
+ * along with this program; if not, write to the Free Software Foundation,
+ * Inc., 51 Franklin Street, Fifth Floor, Boston, MA  02110-1301, USA.
+ */
+package org.sonarsource.sonarlint.plugin.api.module.file;
+
+public interface ModuleFileListener {
+  /**
+   * React to a file creation, deletion or modification event
+   *
+   * @param event an event that concerns a file in the module file system
+   * @since 5.4
+   */
+  void process(ModuleFileEvent event);
+}
