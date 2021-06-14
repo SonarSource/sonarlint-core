2bda04d77fcd76a6b34c0df7322b384184a0b19d
knize <kirill.knize@sonarsource.com>
2021-06-14 15:16:28 +0200

    SLCORE-311 telemetry for secrets

--- core/src/main/java/org/sonarsource/sonarlint/core/telemetry/TelemetryLocalStorage.java
+++ core/src/main/java/org/sonarsource/sonarlint/core/telemetry/TelemetryLocalStorage.java
@@ -23,6 +23,8 @@ import java.time.LocalDate;
 import java.time.LocalDateTime;
 import java.time.OffsetDateTime;
 import java.time.OffsetTime;
+import java.util.Collection;
+import java.util.HashSet;
 import java.util.LinkedHashMap;
 import java.util.Map;
 import javax.annotation.CheckForNull;
@@ -43,12 +45,22 @@ class TelemetryLocalStorage {
   private int showHotspotRequestsCount;
   private int taintVulnerabilitiesInvestigatedLocallyCount;
   private int taintVulnerabilitiesInvestigatedRemotelyCount;
+  private final Collection<String> reportedRules;
 
   TelemetryLocalStorage() {
     enabled = true;
     installTime = OffsetDateTime.now();
     analyzers = new LinkedHashMap<>();
     notificationsCountersByEventType = new LinkedHashMap<>();
+    reportedRules = new HashSet<>();
+  }
+
+  public Collection<String> getReportedRules() {
+    return reportedRules;
+  }
+
+  public void addReportedRule(String reportedRule) {
+    this.reportedRules.add(reportedRule);
   }
 
   @Deprecated
