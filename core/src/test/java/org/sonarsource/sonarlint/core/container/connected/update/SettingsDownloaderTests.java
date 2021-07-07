7840782d60337f040f43da3c4379ea5bfd0fba14
Damien Urruty <damien.urruty@sonarsource.com>
2021-07-07 23:38:48 +0200

    Move settings download code to api package

--- core/src/test/java/org/sonarsource/sonarlint/core/container/connected/update/SettingsDownloaderTests.java
+++ core/src/test/java/org/sonarsource/sonarlint/core/container/connected/update/SettingsDownloaderTests.java
@@ -24,9 +24,6 @@ import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 import org.junit.jupiter.api.extension.RegisterExtension;
 import org.junit.jupiter.api.io.TempDir;
-import org.sonarqube.ws.Settings;
-import org.sonarqube.ws.Settings.FieldValues;
-import org.sonarqube.ws.Settings.FieldValues.Value;
 import org.sonarqube.ws.Settings.Setting;
 import org.sonarqube.ws.Settings.Values;
 import org.sonarqube.ws.Settings.ValuesWsResponse;
@@ -34,8 +31,6 @@ import org.sonarsource.sonarlint.core.MockWebServerExtension;
 import org.sonarsource.sonarlint.core.container.storage.ProtobufUtil;
 import org.sonarsource.sonarlint.core.container.storage.StoragePaths;
 import org.sonarsource.sonarlint.core.proto.Sonarlint.GlobalProperties;
-import org.sonarsource.sonarlint.core.proto.Sonarlint.ProjectConfiguration;
-import org.sonarsource.sonarlint.core.proto.Sonarlint.ProjectConfiguration.Builder;
 
 import static org.assertj.core.api.Assertions.assertThat;
 import static org.assertj.core.api.Assertions.entry;
@@ -75,44 +70,6 @@ class SettingsDownloaderTests {
     assertThat(properties.getPropertiesMap()).containsOnly(
       entry("sonar.core.treemap.sizemetric", "ncloc"),
       entry("views.servers", "135817900907501"));
-  }
-
-  @Test
-  void testFetchProjectSettings() {
-
-    Settings.FieldValues.Value.Builder valuesBuilder = Value.newBuilder();
-    valuesBuilder.putValue("filepattern", "**/*.xml");
-    valuesBuilder.putValue("rulepattern", "*:S12345");
-    Value value1 = valuesBuilder.build();
-    valuesBuilder.clear();
-    valuesBuilder.putValue("filepattern", "**/*.java");
-    valuesBuilder.putValue("rulepattern", "*:S456");
-    Value value2 = valuesBuilder.build();
-
-    ValuesWsResponse response = ValuesWsResponse.newBuilder()
-      .addSettings(Setting.newBuilder()
-        .setKey("sonar.inclusions")
-        .setValues(Values.newBuilder().addValues("**/*.java")))
-      .addSettings(Setting.newBuilder()
-        .setKey("sonar.java.fileSuffixes")
-        .setValue("*.java"))
-      .addSettings(Setting.newBuilder()
-        .setKey("sonar.issue.exclusions.multicriteria")
-        .setFieldValues(FieldValues.newBuilder().addFieldValues(value1).addFieldValues(value2)).build())
-      .build();
-    mockServer.addProtobufResponse("/api/settings/values.protobuf?component=foo", response);
-
-    Builder builder = ProjectConfiguration.newBuilder();
-    underTest.fetchProjectSettings("foo", builder);
-
-    assertThat(builder.getPropertiesMap()).containsOnly(
-      entry("sonar.inclusions", "**/*.java"),
-      entry("sonar.java.fileSuffixes", "*.java"),
-      entry("sonar.issue.exclusions.multicriteria", "1,2"),
-      entry("sonar.issue.exclusions.multicriteria.1.filepattern", "**/*.xml"),
-      entry("sonar.issue.exclusions.multicriteria.1.rulepattern", "*:S12345"),
-      entry("sonar.issue.exclusions.multicriteria.2.filepattern", "**/*.java"),
-      entry("sonar.issue.exclusions.multicriteria.2.rulepattern", "*:S456"));
   }
 
   @Test
