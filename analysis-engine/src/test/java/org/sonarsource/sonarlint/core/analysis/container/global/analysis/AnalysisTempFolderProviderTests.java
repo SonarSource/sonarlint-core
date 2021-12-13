c08f50bd20585550e09711d56335aa26e81b2914
Julien HENRY <julien.henry@sonarsource.com>
2021-12-13 16:39:06 +0100

    Move AnalysisTempFolderProvider to analysis-engine

--- analysis-engine/src/test/java/org/sonarsource/sonarlint/core/analysis/container/global/analysis/AnalysisTempFolderProviderTests.java
+++ analysis-engine/src/test/java/org/sonarsource/sonarlint/core/analysis/container/global/analysis/AnalysisTempFolderProviderTests.java
@@ -1,5 +1,5 @@
 /*
- * SonarLint Core - Implementation
+ * SonarLint Core - Analysis Engine
  * Copyright (C) 2016-2021 SonarSource SA
  * mailto:info AT sonarsource DOT com
  *
@@ -17,17 +17,17 @@
  * along with this program; if not, write to the Free Software Foundation,
  * Inc., 51 Franklin Street, Fifth Floor, Boston, MA  02110-1301, USA.
  */
-package org.sonarsource.sonarlint.core.container.analysis;
+package org.sonarsource.sonarlint.core.analysis.container.global.analysis;
 
-import org.junit.Test;
+import org.junit.jupiter.api.Test;
 import org.sonar.api.utils.TempFolder;
 
 import static org.assertj.core.api.Assertions.assertThatExceptionOfType;
 
-public class AnalysisTempFolderProviderTest {
+class AnalysisTempFolderProviderTests {
 
   @Test
-  public void allMethodsShouldThrow() {
+  void allMethodsShouldThrow() {
     AnalysisTempFolderProvider underTest = new AnalysisTempFolderProvider();
     TempFolder tempFolder = underTest.provide();
 
