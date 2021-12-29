53e153b1222b180835d732e517bb07a454277eff
Damien Urruty <damien.urruty@sonarsource.com>
2021-12-29 14:49:20 +0100

    Remove dead code and fix warnings

--- plugin-commons/src/main/java/org/sonarsource/sonarlint/core/plugin/commons/ExtensionUtils.java
+++ plugin-commons/src/main/java/org/sonarsource/sonarlint/core/plugin/commons/ExtensionUtils.java
@@ -48,7 +48,7 @@ public class ExtensionUtils {
   }
 
   public static boolean isType(Object extension, Class<?> extensionClass) {
-    Class clazz = extension instanceof Class ? (Class) extension : extension.getClass();
+    var clazz = extension instanceof Class ? (Class<?>) extension : extension.getClass();
     return extensionClass.isAssignableFrom(clazz);
   }
 }
