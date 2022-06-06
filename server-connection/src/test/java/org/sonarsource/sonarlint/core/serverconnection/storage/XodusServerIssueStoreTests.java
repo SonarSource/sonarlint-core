d6eaff0cba3b8c1282acb888f197a99ef537d4dd
Damien Urruty <damien.urruty@sonarsource.com>
2022-06-06 18:03:28 +0200

    SLCORE-382 Replace protobuf storage for issues by Xodus

---
+++ server-connection/src/test/java/org/sonarsource/sonarlint/core/serverconnection/storage/XodusServerIssueStoreTests.java
@@ -0,0 +1,211 @@
+/*
+ * SonarLint Core - Server Connection
+ * Copyright (C) 2016-2022 SonarSource SA
+ * mailto:info AT sonarsource DOT com
+ *
+ * This program is free software; you can redistribute it and/or
+ * modify it under the terms of the GNU Lesser General Public
+ * License as published by the Free Software Foundation; either
+ * version 3 of the License, or (at your option) any later version.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the GNU
+ * Lesser General Public License for more details.
+ *
+ * You should have received a copy of the GNU Lesser General Public License
+ * along with this program; if not, write to the Free Software Foundation,
+ * Inc., 51 Franklin Street, Fifth Floor, Boston, MA  02110-1301, USA.
+ */
+package org.sonarsource.sonarlint.core.serverconnection.storage;
+
+import java.nio.file.Path;
+import java.time.Instant;
+import java.util.List;
+import java.util.Set;
+import org.junit.jupiter.api.AfterEach;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.io.TempDir;
+import org.sonarsource.sonarlint.core.serverconnection.ServerIssue;
+
+import static org.assertj.core.api.Assertions.assertThat;
+import static org.assertj.core.api.Assertions.tuple;
+import static org.sonarsource.sonarlint.core.serverconnection.storage.ServerIssueFixtures.aServerIssue;
+
+class XodusServerIssueStoreTests {
+  @TempDir
+  Path tmpBaseDir;
+  private XodusServerIssueStore store;
+
+  @BeforeEach
+  void setUp() {
+    store = new XodusServerIssueStore(tmpBaseDir);
+  }
+
+  @AfterEach
+  void tearDown() {
+    store.close();
+  }
+
+  @Test
+  void should_return_empty_when_issue_id_unknown() {
+    var issue = store.getByKey("unknownKey");
+
+    assertThat(issue).isEmpty();
+  }
+
+  @Test
+  void should_return_empty_when_file_path_unknown() {
+    var issues = store.load("projectKey", "path");
+
+    assertThat(issues).isEmpty();
+  }
+
+  @Test
+  void should_save_an_issue() {
+    var creationDate = Instant.now();
+
+    store
+      .save("projectKey", List.of(aServerIssue().setCreationDate(creationDate).setCodeSnippet("code")
+        .setFlows(List.of(new ServerIssue.Flow(List.of(new ServerIssue.ServerIssueLocation("file/path",
+          new ServerIssue.TextRange(5, 6, 7, 8), "flow message", "code")))))));
+
+    var savedIssueOpt = store.getByKey("key");
+    assertThat(savedIssueOpt).isNotEmpty();
+    var savedIssue = savedIssueOpt.get();
+    assertThat(savedIssue.key()).isEqualTo("key");
+    assertThat(savedIssue.resolved()).isTrue();
+    assertThat(savedIssue.ruleKey()).isEqualTo("repo:key");
+    assertThat(savedIssue.getMessage()).isEqualTo("message");
+    assertThat(savedIssue.lineHash()).isEqualTo("hash");
+    assertThat(savedIssue.getFilePath()).isEqualTo("file/path");
+    assertThat(savedIssue.creationDate()).isEqualTo(creationDate);
+    assertThat(savedIssue.severity()).isEqualTo("MINOR");
+    assertThat(savedIssue.type()).isEqualTo("BUG");
+    assertThat(savedIssue.getTextRange().getStartLine()).isEqualTo(1);
+    assertThat(savedIssue.getTextRange().getStartLineOffset()).isEqualTo(2);
+    assertThat(savedIssue.getTextRange().getEndLine()).isEqualTo(3);
+    assertThat(savedIssue.getTextRange().getEndLineOffset()).isEqualTo(4);
+    assertThat(savedIssue.getCodeSnippet()).isEqualTo("code");
+    assertThat(savedIssue.getFlows()).hasSize(1);
+    assertThat(savedIssue.getFlows().get(0).locations())
+      .extracting("filePath", "message", "codeSnippet", "textRange.startLine", "textRange.startLineOffset", "textRange.endLine", "textRange.endLineOffset")
+      .containsOnly(tuple("file/path", "flow message", "code", 5, 6, 7, 8));
+  }
+
+  @Test
+  void should_load_all_issues_of_a_file() {
+    store.save("projectKey", List.of(
+      aServerIssue().setFilePath("file/path1").setKey("key1"),
+      aServerIssue().setFilePath("file/path2").setKey("key2"),
+      aServerIssue().setFilePath("file/path1").setKey("key3")));
+
+    var issues = store.load("projectKey", "file/path1");
+    assertThat(issues)
+      .extracting(ServerIssue::key)
+      .containsOnly("key1", "key3");
+  }
+
+  @Test
+  void should_load_issues_of_the_right_file() {
+    store.save("projectKey", List.of(aServerIssue().setFilePath("file/path1").setKey("key1")));
+    store.save("projectKey2", List.of(aServerIssue().setFilePath("file/path1").setKey("key2")));
+
+    var issues = store.load("projectKey", "file/path1");
+    assertThat(issues)
+      .extracting(ServerIssue::key)
+      .containsOnly("key1");
+  }
+
+  @Test
+  void should_remove_issues_by_key() {
+    store.save("projectKey", List.of(
+      aServerIssue().setKey("key1"),
+      aServerIssue().setKey("key2"),
+      aServerIssue().setKey("key3")));
+
+    store.removeAll(Set.of("key1", "key3"));
+
+    assertThat(store.getByKey("key1")).isEmpty();
+    assertThat(store.getByKey("key3")).isEmpty();
+    assertThat(store.getByKey("key2")).isNotEmpty();
+    assertThat(store.load("projectKey", "file/path"))
+      .extracting(ServerIssue::key)
+      .containsOnly("key2");
+  }
+
+  @Test
+  void should_save_issue_without_range() {
+    store.save("projectKey", List.of(
+      aServerIssue().setKey("key1").setTextRange(null)));
+
+    var issue = store.getByKey("key1");
+
+    assertThat(issue).isNotEmpty();
+    assertThat(issue.get().getTextRange()).isNull();
+  }
+
+  @Test
+  void should_save_issue_with_line_only() {
+    store.save("projectKey", List.of(
+      aServerIssue().setKey("key1").setTextRange(new ServerIssue.TextRange(3))));
+
+    var issue = store.getByKey("key1");
+
+    assertThat(issue).isNotEmpty();
+    assertThat(issue.get().getTextRange().getStartLine()).isEqualTo(3);
+  }
+
+  @Test
+  void should_replace_existing_issue_with_same_key() {
+    store.save("projectKey", List.of(aServerIssue().setKey("key1").setFilePath("filePath").setMessage("old message")));
+    store.save("projectKey", List.of(aServerIssue().setKey("key1").setFilePath("filePath").setMessage("new message")));
+
+    var issues = store.load("projectKey", "filePath");
+
+    assertThat(issues)
+      .extracting("key", "message")
+      .containsOnly(tuple("key1", "new message"));
+  }
+
+  @Test
+  void should_save_from_different_files() {
+    store.save("projectKey", List.of(
+      aServerIssue().setKey("key1").setFilePath("filePath1"),
+      aServerIssue().setKey("key2").setFilePath("filePath2")));
+
+    var issues = store.load("projectKey", "filePath1");
+
+    assertThat(issues)
+      .extracting("key", "message")
+      .containsOnly(tuple("key1", "message"));
+  }
+
+  @Test
+  void should_save_issues_with_different_case_keys() {
+    store.save("projectKey", List.of(
+      aServerIssue().setKey("key"),
+      aServerIssue().setKey("Key")));
+
+    var issues = store.load("projectKey", "file/path");
+
+    assertThat(issues)
+      .extracting("key")
+      .containsOnly("key", "Key");
+  }
+
+  @Test
+  void should_move_issue_to_other_file() {
+    store.save("projectKey", List.of(
+      aServerIssue().setKey("key").setFilePath("filePath1")));
+    store.save("projectKey", List.of(
+      aServerIssue().setKey("key").setFilePath("filePath2")));
+
+    var issuesFile1 = store.load("projectKey", "filePath1");
+    assertThat(issuesFile1).isEmpty();
+
+    var issuesFile2 = store.load("projectKey", "filePath2");
+    assertThat(issuesFile2).isNotEmpty();
+  }
+}
