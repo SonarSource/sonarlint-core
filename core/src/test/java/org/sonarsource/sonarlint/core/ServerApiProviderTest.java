de5418b7601961d8cad4c6832750ce1dbc1e83b4
Julien HENRY <julien.henry@sonarsource.com>
2022-10-25 14:08:36 +0200

    Add tests for ServerApiProvider

---
+++ core/src/test/java/org/sonarsource/sonarlint/core/ServerApiProviderTest.java
@@ -0,0 +1,86 @@
+/*
+ * SonarLint Core - Implementation
+ * Copyright (C) 2016-2022 SonarSource SA
+ * mailto:info AT sonarsource DOT com
+ *
+ * This program is free software; you can redistribute it and/or
+ * modify it under the terms of the GNU Lesser General Public
+ * License as published by the Free Software Foundation; either
+ * version 3 of the License, or (at your option) any later version.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the GNU
+ * Lesser General Public License for more details.
+ *
+ * You should have received a copy of the GNU Lesser General Public License
+ * along with this program; if not, write to the Free Software Foundation,
+ * Inc., 51 Franklin Street, Fifth Floor, Boston, MA  02110-1301, USA.
+ */
+package org.sonarsource.sonarlint.core;
+
+import java.util.Optional;
+import org.assertj.core.api.Assertions;
+import org.junit.jupiter.api.Test;
+import org.sonarsource.sonarlint.core.clientapi.SonarLintClient;
+import org.sonarsource.sonarlint.core.commons.http.HttpClient;
+import org.sonarsource.sonarlint.core.repository.connection.ConnectionConfigurationRepository;
+import org.sonarsource.sonarlint.core.repository.connection.SonarCloudConnectionConfiguration;
+import org.sonarsource.sonarlint.core.repository.connection.SonarQubeConnectionConfiguration;
+import org.sonarsource.sonarlint.core.serverapi.ServerApi;
+
+import static org.assertj.core.api.Assertions.assertThat;
+import static org.junit.jupiter.api.Assertions.*;
+import static org.mockito.Mockito.mock;
+import static org.mockito.Mockito.when;
+
+class ServerApiProviderTest {
+
+
+  private final ConnectionConfigurationRepository connectionRepository = mock(ConnectionConfigurationRepository.class);
+  private final SonarLintClient client = mock(SonarLintClient.class);
+  private final ServerApiProvider underTest = new ServerApiProvider(connectionRepository, client);
+
+  @Test
+  void getServerApi_for_sonarqube() {
+    when(connectionRepository.getConnectionById("sq1")).thenReturn(new SonarQubeConnectionConfiguration("sq1", "http://mysonarqube.org"));
+    var httpClient = mock(HttpClient.class);
+    when(client.getHttpClient("sq1")).thenReturn(httpClient);
+
+    var serverApi = underTest.getServerApi("sq1");
+
+    assertThat(serverApi).isPresent();
+  }
+
+  @Test
+  void getServerApi_for_sonarcloud() {
+    when(connectionRepository.getConnectionById("sc1")).thenReturn(new SonarCloudConnectionConfiguration("sc1", "myorg"));
+    var httpClient = mock(HttpClient.class);
+    when(client.getHttpClient("sc1")).thenReturn(httpClient);
+
+    var serverApi = underTest.getServerApi("sc1");
+
+    assertThat(serverApi).isPresent();
+  }
+
+  @Test
+  void getServerApi_returns_empty_if_connection_doesnt_exists() {
+    when(connectionRepository.getConnectionById("sc1")).thenReturn(null);
+    var httpClient = mock(HttpClient.class);
+    when(client.getHttpClient("sc1")).thenReturn(httpClient);
+
+    var serverApi = underTest.getServerApi("sc1");
+
+    assertThat(serverApi).isEmpty();
+  }
+
+  @Test
+  void getServerApi_returns_empty_if_client_cant_provide_httpclient() {
+    when(connectionRepository.getConnectionById("sc1")).thenReturn(new SonarCloudConnectionConfiguration("sc1", "myorg"));
+    when(client.getHttpClient("sc1")).thenReturn(null);
+
+    var serverApi = underTest.getServerApi("sc1");
+
+    assertThat(serverApi).isEmpty();
+  }
+}
