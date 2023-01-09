8ce0fe7a12fb5729cce93b5ffc481c6a3747b2ed
Julien HENRY <julien.henry@sonarsource.com>
2023-01-09 16:14:03 +0100

    Group ITs to limit number of SonarQube startup

--- its/tests/src/test/java/its/SonarQubeCommunityEditionTests.java
+++ its/tests/src/test/java/its/SonarQubeCommunityEditionTests.java
@@ -23,6 +23,7 @@ import com.sonar.orchestrator.OrchestratorExtension;
 import com.sonar.orchestrator.build.SonarScanner;
 import com.sonar.orchestrator.locator.FileLocation;
 import com.sonar.orchestrator.locator.MavenLocation;
+import its.utils.ItUtils;
 import its.utils.OrchestratorUtils;
 import java.io.IOException;
 import java.nio.file.Path;
@@ -31,10 +32,14 @@ import java.util.ArrayList;
 import java.util.HashMap;
 import java.util.List;
 import java.util.Map;
+import java.util.stream.Collectors;
 import org.junit.jupiter.api.AfterEach;
 import org.junit.jupiter.api.BeforeAll;
 import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Nested;
 import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.TestInstance;
+import org.junit.jupiter.api.TestInstance.Lifecycle;
 import org.junit.jupiter.api.extension.RegisterExtension;
 import org.junit.jupiter.api.io.TempDir;
 import org.sonarqube.ws.Issues.Issue;
@@ -54,13 +59,15 @@ import org.sonarsource.sonarlint.core.serverconnection.issues.ServerIssue;
 import static its.utils.ItUtils.SONAR_VERSION;
 import static org.assertj.core.api.Assertions.assertThat;
 
-class ConnectedCommunityIssueDownloadTests extends AbstractConnectedTests {
+class SonarQubeCommunityEditionTests extends AbstractConnectedTests {
   private static final String PROJECT_KEY = "sample-xoo";
 
   @RegisterExtension
   static OrchestratorExtension ORCHESTRATOR = OrchestratorUtils.defaultEnvBuilder()
     .addPlugin(MavenLocation.of("org.sonarsource.sonarqube", "sonar-xoo-plugin", SONAR_VERSION))
     .restoreProfileAtStartup(FileLocation.ofClasspath("/xoo-sonarlint.xml"))
+    .setServerProperty("sonar.projectCreation.mainBranchName", MAIN_BRANCH_NAME)
+    .keepBundledPlugins()
     .build();
 
   private ConnectedSonarLintEngine engine;
@@ -147,6 +154,78 @@ class ConnectedCommunityIssueDownloadTests extends AbstractConnectedTests {
 
     // No hotspots
     assertThat(allIssues.values()).allSatisfy(i -> assertThat(i.getType()).isIn(RuleType.CODE_SMELL, RuleType.BUG, RuleType.VULNERABILITY));
+  }
+
+  @Nested
+  // TODO Can be removed when switching to Java 16+ and changing prepare() to static
+  @TestInstance(Lifecycle.PER_CLASS)
+  class PathPrefix {
+    private static final String MULTI_MODULE_PROJECT_KEY = "com.sonarsource.it.samples:multi-modules-sample";
+
+    @BeforeAll
+    void analyzeMultiModuleProject() {
+      // Project has 5 modules: B, B/B1, B/B2, A, A/A1 and A/A2
+      analyzeMavenProject(ORCHESTRATOR, "multi-modules-sample");
+    }
+    @Test
+    void should_apply_path_prefixes_when_importing_entire_project() throws IOException {
+      engine.updateProject(endpointParams(ORCHESTRATOR), sqHttpClient(), MULTI_MODULE_PROJECT_KEY, null);
+
+      // entire project imported in IDE
+      var projectDir = Paths.get("projects/multi-modules-sample").toAbsolutePath();
+      var ideFiles = ItUtils.collectAllFiles(projectDir).stream()
+        .map(f -> projectDir.relativize(f).toString())
+        .collect(Collectors.toList());
+
+      var projectBinding = engine.calculatePathPrefixes(MULTI_MODULE_PROJECT_KEY, ideFiles);
+      assertThat(projectBinding.serverPathPrefix()).isEmpty();
+      assertThat(projectBinding.idePathPrefix()).isEmpty();
+      engine.downloadAllServerIssuesForFile(endpointParams(ORCHESTRATOR), sqHttpClient(), projectBinding,
+        "module_b/module_b1/src/main/java/com/sonar/it/samples/modules/b1/HelloB1.java", MAIN_BRANCH_NAME, null);
+      var serverIssues = engine.getServerIssues(projectBinding, MAIN_BRANCH_NAME, "module_b/module_b1/src/main/java/com/sonar/it/samples/modules/b1/HelloB1.java");
+      if (ORCHESTRATOR.getServer().version().isGreaterThanOrEquals(9, 6)) {
+        assertThat(serverIssues).isEmpty();
+        assertThat(logs).contains("Skip downloading file issues on SonarQube 9.6+");
+      } else {
+        assertThat(serverIssues).hasSize(2);
+      }
+      engine.syncServerIssues(endpointParams(ORCHESTRATOR), sqHttpClient(), MULTI_MODULE_PROJECT_KEY, MAIN_BRANCH_NAME, null);
+      if (!ORCHESTRATOR.getServer().version().isGreaterThanOrEquals(9, 6)) {
+        assertThat(logs).contains("Incremental issue sync is not supported. Skipping.");
+      }
+      serverIssues = engine.getServerIssues(projectBinding, MAIN_BRANCH_NAME, "module_b/module_b1/src/main/java/com/sonar/it/samples/modules/b1/HelloB1.java");
+      assertThat(serverIssues).hasSize(2);
+    }
+
+    @Test
+    void should_apply_path_prefixes_when_importing_module() throws IOException {
+      engine.updateProject(endpointParams(ORCHESTRATOR), sqHttpClient(), MULTI_MODULE_PROJECT_KEY, null);
+
+      // only module B1 imported in IDE
+      var projectDirB1 = Paths.get("projects/multi-modules-sample/module_b/module_b1").toAbsolutePath();
+      var ideFiles = ItUtils.collectAllFiles(projectDirB1).stream()
+        .map(f -> projectDirB1.relativize(f).toString())
+        .collect(Collectors.toList());
+
+      var projectBinding = engine.calculatePathPrefixes(MULTI_MODULE_PROJECT_KEY, ideFiles);
+      assertThat(projectBinding.serverPathPrefix()).isEqualTo("module_b/module_b1");
+      assertThat(projectBinding.idePathPrefix()).isEmpty();
+      engine.downloadAllServerIssuesForFile(endpointParams(ORCHESTRATOR), sqHttpClient(), projectBinding,
+        "src/main/java/com/sonar/it/samples/modules/b1/HelloB1.java", MAIN_BRANCH_NAME, null);
+      var serverIssues = engine.getServerIssues(projectBinding, MAIN_BRANCH_NAME, "src/main/java/com/sonar/it/samples/modules/b1/HelloB1.java");
+      if (ORCHESTRATOR.getServer().version().isGreaterThanOrEquals(9, 6)) {
+        assertThat(serverIssues).isEmpty();
+        assertThat(logs).contains("Skip downloading file issues on SonarQube 9.6+");
+      } else {
+        assertThat(serverIssues).hasSize(2);
+      }
+      engine.syncServerIssues(endpointParams(ORCHESTRATOR), sqHttpClient(), MULTI_MODULE_PROJECT_KEY, MAIN_BRANCH_NAME, null);
+      if (!ORCHESTRATOR.getServer().version().isGreaterThanOrEquals(9, 6)) {
+        assertThat(logs).contains("Incremental issue sync is not supported. Skipping.");
+      }
+      serverIssues = engine.getServerIssues(projectBinding, MAIN_BRANCH_NAME, "src/main/java/com/sonar/it/samples/modules/b1/HelloB1.java");
+      assertThat(serverIssues).hasSize(2);
+    }
   }
 
   private static void analyzeProject(String projectDirName) {
