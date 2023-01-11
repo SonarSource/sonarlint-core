3ae27b1ba72631829474bbce08bf58a74d1a9641
Mithil <mithil.dobarkar@sonarsource.com>
2023-01-11 15:52:30 +0100

    SLCORE-435 Split line for recommended characters

--- client-api/src/main/java/org/sonarsource/sonarlint/core/clientapi/backend/rules/OthersSectionHtmlContent.java
+++ client-api/src/main/java/org/sonarsource/sonarlint/core/clientapi/backend/rules/OthersSectionHtmlContent.java
@@ -25,7 +25,8 @@ public class OthersSectionHtmlContent {
 
   public static final String HTML_CONTENT =
     "<h2>How can I fix it in another component or framework?</h2>\n" +
-    "<p>Although the main framework or component you use in your project is not listed, you may find helpful content in the instructions we provide.</p>\n" +
+    "<p>Although the main framework or component you use in your project is not listed, " +
+      "you may find helpful content in the instructions we provide.</p>\n" +
     "<p>Caution: The libraries mentioned in these instructions may not be appropriate for your code.</p>\n" +
     "<p>\n" +
     "<ul>\n" +
@@ -33,7 +34,8 @@ public class OthersSectionHtmlContent {
     "<li>Don't blindly copy and paste the fix ups into your code.</li>\n" +
     "</ul>\n" +
     "<h2>Help us improve</h2>\n" +
-    "<p>Let us know if the instructions we provide do not work for you. Tell us which framework you use and why our solution does not work by submitting an idea on the SonarLint product-board.</p>\n" +
+    "<p>Let us know if the instructions we provide do not work for you. " +
+      "Tell us which framework you use and why our solution does not work by submitting an idea on the SonarLint product-board.</p>\n" +
     "<a href=\"https://portal.productboard.com/sonarsource/4-sonarlint/submit-idea\">Submit an idea</a>\n" +
     "<p>We will do our best to provide you with more relevant instructions in the future.</p>";
 
