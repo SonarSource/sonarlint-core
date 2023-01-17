171ca4d75c24033e115a81bd7481427cd1f39f4c
Julien HENRY <julien.henry@sonarsource.com>
2023-01-17 09:15:49 +0100

    Update parent pom and license headers

--- rule-extractor/src/main/java/org/sonarsource/sonarlint/core/rule/extractor/SonarLintRuleParamType.java
+++ rule-extractor/src/main/java/org/sonarsource/sonarlint/core/rule/extractor/SonarLintRuleParamType.java
@@ -1,6 +1,6 @@
 /*
  * SonarLint Core - Rule Extractor
- * Copyright (C) 2016-2022 SonarSource SA
+ * Copyright (C) 2016-2023 SonarSource SA
  * mailto:info AT sonarsource DOT com
  *
  * This program is free software; you can redistribute it and/or
--- server-connection/src/main/java/org/sonarsource/sonarlint/core/serverconnection/storage/TarGzUtils.java
+++ server-connection/src/main/java/org/sonarsource/sonarlint/core/serverconnection/storage/TarGzUtils.java
@@ -1,6 +1,6 @@
 /*
  * SonarLint Core - Server Connection
- * Copyright (C) 2016-2022 SonarSource SA
+ * Copyright (C) 2016-2023 SonarSource SA
  * mailto:info AT sonarsource DOT com
  *
  * This program is free software; you can redistribute it and/or
@@ -17,7 +17,6 @@
  * along with this program; if not, write to the Free Software Foundation,
  * Inc., 51 Franklin Street, Fifth Floor, Boston, MA  02110-1301, USA.
  */
-
 package org.sonarsource.sonarlint.core.serverconnection.storage;
 
 import java.io.BufferedInputStream;
