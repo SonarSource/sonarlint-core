5517305a379ae2ae20465b0665d139411b1e7c66
Vojtech Suchy <vojtech.suchy@sonarsource.com>
2023-05-04 14:44:40 +0200

    Update core/src/main/java/org/sonarsource/sonarlint/core/BindingSuggestionProvider.java
    Accept review comment to be inline with spec
    
    Co-authored-by: Julien HENRY <julien.henry@sonarsource.com>

--- core/src/main/java/org/sonarsource/sonarlint/core/BindingSuggestionProvider.java
+++ core/src/main/java/org/sonarsource/sonarlint/core/BindingSuggestionProvider.java
@@ -176,7 +176,7 @@ public class BindingSuggestionProvider {
           searchGoodMatchInConnections(suggestions, configScopeName, bindingClueWithConnections.getConnectionIds());
         }
         if (cluesWithoutProjectKey.isEmpty()) {
-          searchGoodMatchInConnections(suggestions, configScopeName, eligibleConnectionIds);
+          searchGoodMatchInConnections(suggestions, configScopeName, candidateConnectionIds);
         }
       }
     }
