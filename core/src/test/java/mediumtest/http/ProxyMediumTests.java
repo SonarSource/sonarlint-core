5453a6e9b49a9ccf7e1537daeca9eda5f17b6438
Nicolas QUINQUENEL <nicolas.quinquenel@sonarsource.com>
2023-08-07 09:26:25 +0200

    Medium test for proxy outside valid port range (#672)

--- core/src/test/java/mediumtest/http/ProxyMediumTests.java
+++ core/src/test/java/mediumtest/http/ProxyMediumTests.java
@@ -35,11 +35,11 @@ import org.junit.jupiter.api.extension.RegisterExtension;
 import org.sonarsource.sonarlint.core.clientapi.backend.rules.EffectiveRuleDetailsDto;
 import org.sonarsource.sonarlint.core.clientapi.backend.rules.GetEffectiveRuleDetailsParams;
 import org.sonarsource.sonarlint.core.commons.Language;
+import org.sonarsource.sonarlint.core.commons.log.SonarLintLogTester;
 import org.sonarsource.sonarlint.core.serverapi.proto.sonarqube.ws.Common;
 import org.sonarsource.sonarlint.core.serverapi.proto.sonarqube.ws.Rules;
 
 import static com.github.tomakehurst.wiremock.client.WireMock.aResponse;
-import static com.github.tomakehurst.wiremock.client.WireMock.containing;
 import static com.github.tomakehurst.wiremock.client.WireMock.equalTo;
 import static com.github.tomakehurst.wiremock.client.WireMock.get;
 import static com.github.tomakehurst.wiremock.client.WireMock.getRequestedFor;
@@ -66,6 +66,9 @@ class ProxyMediumTests {
   static WireMockExtension proxyMock = WireMockExtension.newInstance()
     .options(wireMockConfig().dynamicPort())
     .build();
+
+  @RegisterExtension
+  SonarLintLogTester logTester = new SonarLintLogTester();
 
   @BeforeEach
   void configureProxy(TestInfo info) {
@@ -175,6 +178,35 @@ class ProxyMediumTests {
 
     proxyMock.verify(getRequestedFor(urlEqualTo("/api/rules/show.protobuf?key=python:S139"))
       .withHeader("Proxy-Authorization", equalTo("Basic " + Base64.getEncoder().encodeToString((proxyLogin + ":").getBytes(StandardCharsets.UTF_8)))));
+  }
+
+  @Test
+  @Tag(PROXY_AUTH_ENABLED)
+  void it_should_fail_if_proxy_port_is_not_valid() {
+    var proxyLogin = "proxyLogin";
+    var fakeClient = newFakeClient()
+      .withHttpProxy("localhost", 70000)
+      .withHttpProxyAuth(proxyLogin, null)
+      .build();
+    backend = newBackend()
+      .withSonarQubeConnection("connectionId", sonarqubeMock.baseUrl(), storage -> storage.withProject("projectKey",
+        projectStorage -> projectStorage.withRuleSet(Language.PYTHON.getLanguageKey(),
+          ruleSet -> ruleSet.withActiveRule("python:S139", "INFO", Map.of("legalTrailingCommentPattern", "blah")))))
+      .withBoundConfigScope("scopeId", "connectionId", "projectKey")
+      .withConnectedEmbeddedPluginAndEnabledLanguage(TestPlugin.PYTHON)
+      .build(fakeClient);
+    sonarqubeMock.stubFor(get("/api/rules/show.protobuf?key=python:S139")
+      .willReturn(aResponse().withStatus(200).withResponseBody(protobufBody(Rules.ShowResponse.newBuilder()
+        .setRule(Rules.Rule.newBuilder().setName("newName").setSeverity("INFO").setType(Common.RuleType.BUG).setLang("py").setHtmlDesc(
+          "desc").setHtmlNote("extendedDesc from server").build())
+        .build()))));
+
+    var details = getEffectiveRuleDetails("scopeId", "python:S139");
+
+    assertThat(details.getDescription().getLeft().getHtmlContent()).contains("extendedDesc from server");
+
+    assertThat(logTester.logs()).contains("Unable to get proxy");
+    assertThat(logTester.logs()).anyMatch(s -> s.contains("Port is outside the valid range for hostname: localhost"));
   }
 
   private EffectiveRuleDetailsDto getEffectiveRuleDetails(String configScopeId, String ruleKey) {
