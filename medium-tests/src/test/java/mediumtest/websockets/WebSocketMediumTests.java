ff32f81c0335e6d27c605e4eda94b9c5535db7f1
Julien HENRY <julien.henry@sonarsource.com>
2023-10-25 10:54:22 +0200

    Change WebSocketService to create the connection async

--- medium-tests/src/test/java/mediumtest/websockets/WebSocketMediumTests.java
+++ medium-tests/src/test/java/mediumtest/websockets/WebSocketMediumTests.java
@@ -31,8 +31,10 @@ import org.junit.jupiter.api.AfterEach;
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Nested;
 import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.extension.RegisterExtension;
 import org.sonarsource.sonarlint.core.commons.RuleType;
 import org.sonarsource.sonarlint.core.commons.TextRangeWithHash;
+import org.sonarsource.sonarlint.core.commons.log.SonarLintLogTester;
 import org.sonarsource.sonarlint.core.rpc.protocol.backend.config.binding.BindingConfigurationDto;
 import org.sonarsource.sonarlint.core.rpc.protocol.backend.config.binding.DidUpdateBindingParams;
 import org.sonarsource.sonarlint.core.rpc.protocol.backend.config.scope.DidRemoveConfigurationScopeParams;
@@ -464,6 +466,10 @@ class WebSocketMediumTests {
 
   @Nested
   class WhenConnectionAdded {
+
+    @RegisterExtension
+    SonarLintLogTester logTester = new SonarLintLogTester();
+
     @Test
     void should_subscribe_all_projects_bound_to_added_connection() {
       startWebSocketServer();
@@ -477,6 +483,31 @@ class WebSocketMediumTests {
 
       backend.getConnectionService()
         .didUpdateConnections(new DidUpdateConnectionsParams(emptyList(), List.of(new SonarCloudConnectionConfigurationDto("connectionId", "orgKey", false))));
+
+      await().atMost(Duration.ofSeconds(2)).untilAsserted(() -> assertThat(webSocketServer.getConnections())
+        .extracting(WebSocketConnection::isOpened, WebSocketConnection::getReceivedMessages)
+        .containsExactly(tuple(true, webSocketPayloadBuilder().subscribeWithProjectKey("projectKey").build())));
+    }
+
+    @Test
+    void should_log_failure_and_reconnect_later_if_server_unavailable() {
+      System.setProperty("sonarlint.internal.sonarcloud.websocket.url", "wss://not-found:1234");
+      var client = newFakeClient()
+        .withToken("connectionId", "token")
+        .build();
+      backend = newBackend()
+        .withServerSentEventsEnabled()
+        .withBoundConfigScope("configScope", "connectionId", "projectKey")
+        .build(client);
+
+      backend.getConnectionService()
+        .didUpdateConnections(new DidUpdateConnectionsParams(emptyList(), List.of(new SonarCloudConnectionConfigurationDto("connectionId", "orgKey", false))));
+
+      await().untilAsserted(() -> assertThat(logTester.logs()).contains("Error while trying to create websocket connection for wss://not-found:1234"));
+
+      startWebSocketServer();
+      // Emulate a change on the connection to force websocket service to reconnect
+      backend.getConnectionService().didChangeCredentials(new DidChangeCredentialsParams("connectionId"));
 
       await().atMost(Duration.ofSeconds(2)).untilAsserted(() -> assertThat(webSocketServer.getConnections())
         .extracting(WebSocketConnection::isOpened, WebSocketConnection::getReceivedMessages)
