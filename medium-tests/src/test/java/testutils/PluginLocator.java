1b42a928f0a07ce08e514aa5c1f417d44e1f8ae6
Julien HENRY <julien.henry@sonarsource.com>
2023-11-10 11:03:22 +0100

    SLCORE-552 Update to Spring 5.3.30

--- medium-tests/src/test/java/testutils/PluginLocator.java
+++ medium-tests/src/test/java/testutils/PluginLocator.java
@@ -1,6 +1,6 @@
 /*
  * SonarLint Core - Medium Tests
- * Copyright (C) 2016-2024 SonarSource SA
+ * Copyright (C) 2016-2023 SonarSource SA
  * mailto:info AT sonarsource DOT com
  *
  * This program is free software; you can redistribute it and/or
@@ -17,7 +17,6 @@
  * along with this program; if not, write to the Free Software Foundation,
  * Inc., 51 Franklin Street, Fifth Floor, Boston, MA  02110-1301, USA.
  */
-
 package testutils;
 
 import java.nio.file.Files;
--- rule-extractor/src/main/java/org/sonarsource/sonarlint/core/rule/extractor/package-info.java
+++ rule-extractor/src/main/java/org/sonarsource/sonarlint/core/rule/extractor/package-info.java
@@ -1,6 +1,6 @@
 /*
  * SonarLint Core - Rule Extractor
- * Copyright (C) 2016-2024 SonarSource SA
+ * Copyright (C) 2016-2023 SonarSource SA
  * mailto:info AT sonarsource DOT com
  *
  * This program is free software; you can redistribute it and/or
