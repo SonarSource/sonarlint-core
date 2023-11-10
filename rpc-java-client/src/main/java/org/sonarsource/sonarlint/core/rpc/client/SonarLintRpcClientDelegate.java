3b08ec5c34477a39a45a8862ad9ab0e0451c747b
Julien HENRY <julien.henry@sonarsource.com>
2023-11-10 16:01:44 +0100

    Create a delegate interface for clients, to isolate from lsp4j

--- rpc-java-client/src/main/java/org/sonarsource/sonarlint/core/rpc/client/SonarLintRpcClientDelegate.java
+++ rpc-java-client/src/main/java/org/sonarsource/sonarlint/core/rpc/client/SonarLintRpcClientDelegate.java
@@ -1,5 +1,5 @@
 /*
- * SonarLint Core - RPC Protocol
+ * SonarLint Core - RPC Java Client
  * Copyright (C) 2016-2023 SonarSource SA
  * mailto:info AT sonarsource DOT com
  *
@@ -17,14 +17,25 @@
  * along with this program; if not, write to the Free Software Foundation,
  * Inc., 51 Franklin Street, Fifth Floor, Boston, MA  02110-1301, USA.
  */
-package org.sonarsource.sonarlint.core.rpc.protocol;
+package org.sonarsource.sonarlint.core.rpc.client;
 
-import java.util.concurrent.CompletableFuture;
+import java.net.Authenticator;
+import java.net.InetSocketAddress;
+import java.net.MalformedURLException;
+import java.net.Proxy;
+import java.net.ProxySelector;
+import java.net.URI;
+import java.net.URL;
+import java.util.concurrent.CancellationException;
+import java.util.stream.Collectors;
+import org.eclipse.lsp4j.jsonrpc.CancelChecker;
+import org.eclipse.lsp4j.jsonrpc.ResponseErrorException;
+import org.eclipse.lsp4j.jsonrpc.messages.ResponseError;
 import org.eclipse.lsp4j.jsonrpc.messages.ResponseErrorCode;
-import org.eclipse.lsp4j.jsonrpc.services.JsonNotification;
-import org.eclipse.lsp4j.jsonrpc.services.JsonRequest;
+import org.sonarsource.sonarlint.core.rpc.protocol.SonarLintRpcServer;
 import org.sonarsource.sonarlint.core.rpc.protocol.backend.initialize.ClientInfoDto;
 import org.sonarsource.sonarlint.core.rpc.protocol.backend.initialize.InitializeParams;
+import org.sonarsource.sonarlint.core.rpc.protocol.client.ClientErrorCode;
 import org.sonarsource.sonarlint.core.rpc.protocol.client.OpenUrlInBrowserParams;
 import org.sonarsource.sonarlint.core.rpc.protocol.client.binding.AssistBindingParams;
 import org.sonarsource.sonarlint.core.rpc.protocol.client.binding.AssistBindingResponse;
@@ -46,6 +57,7 @@ import org.sonarsource.sonarlint.core.rpc.protocol.client.http.CheckServerTruste
 import org.sonarsource.sonarlint.core.rpc.protocol.client.http.CheckServerTrustedResponse;
 import org.sonarsource.sonarlint.core.rpc.protocol.client.http.GetProxyPasswordAuthenticationParams;
 import org.sonarsource.sonarlint.core.rpc.protocol.client.http.GetProxyPasswordAuthenticationResponse;
+import org.sonarsource.sonarlint.core.rpc.protocol.client.http.ProxyDto;
 import org.sonarsource.sonarlint.core.rpc.protocol.client.http.SelectProxiesParams;
 import org.sonarsource.sonarlint.core.rpc.protocol.client.http.SelectProxiesResponse;
 import org.sonarsource.sonarlint.core.rpc.protocol.client.info.GetClientInfoResponse;
@@ -58,25 +70,20 @@ import org.sonarsource.sonarlint.core.rpc.protocol.client.progress.StartProgress
 import org.sonarsource.sonarlint.core.rpc.protocol.client.smartnotification.ShowSmartNotificationParams;
 import org.sonarsource.sonarlint.core.rpc.protocol.client.sync.DidSynchronizeConfigurationScopeParams;
 
-public interface SonarLintRpcClient {
+public interface SonarLintRpcClientDelegate {
 
-  @JsonNotification
   void suggestBinding(SuggestBindingParams params);
 
-  @JsonRequest
-  CompletableFuture<FindFileByNamesInScopeResponse> findFileByNamesInScope(FindFileByNamesInScopeParams params);
+  FindFileByNamesInScopeResponse findFileByNamesInScope(FindFileByNamesInScopeParams params, CancelChecker cancelChecker);
 
-  @JsonNotification
   void openUrlInBrowser(OpenUrlInBrowserParams params);
 
   /**
    * Display a message to the user, usually in a small notification.
    * The message is informative and does not imply applying an action.
    */
-  @JsonNotification
   void showMessage(ShowMessageParams params);
 
-  @JsonNotification
   void log(LogParams params);
 
   /**
@@ -85,91 +92,104 @@ public interface SonarLintRpcClient {
    * The one-time mechanism should be handled on the client side (via a "Don't show again" button for example).
    * There is an in-memory cache for the pair of connection ID + version that were already seen on the core side, but it is cleared after each restart.
    */
-  @JsonNotification
   void showSoonUnsupportedMessage(ShowSoonUnsupportedMessageParams params);
 
-  @JsonNotification
   void showSmartNotification(ShowSmartNotificationParams params);
 
   /**
    * Ask the client to provide the dynamic information that can change during the runtime. Static information are provided during {@link SonarLintRpcServer#initialize(InitializeParams)}
    * in {@link ClientInfoDto}
    */
-  @JsonRequest
-  CompletableFuture<GetClientInfoResponse> getClientInfo();
+  GetClientInfoResponse getClientInfo(CancelChecker cancelChecker);
 
-  @JsonNotification
   void showHotspot(ShowHotspotParams params);
 
   /**
    * Sends a notification to the client to show a specific issue (specified by {@link ShowIssueParams}) in the IDE
    */
-  @JsonNotification
   void showIssue(ShowIssueParams params);
 
   /**
    * Can be triggered by the backend when trying to handle a feature that needs a connection, e.g. open hotspot.
    * @return the response to this connection creation assist request, that contains the new connection. The client can cancel the request if the user stops the creation process.
-   * When cancelling the request from the client side, the error code should be {@link org.eclipse.lsp4j.jsonrpc.messages.ResponseErrorCode#ServerCancelled}
+   * @throws java.util.concurrent.CancellationException if the client cancels the process
    */
-  @JsonRequest
-  CompletableFuture<AssistCreatingConnectionResponse> assistCreatingConnection(AssistCreatingConnectionParams params);
+  default AssistCreatingConnectionResponse assistCreatingConnection(AssistCreatingConnectionParams params, CancelChecker cancelChecker) {
+    throw new CancellationException("Not implemented");
+  }
 
   /**
    * Can be triggered by the backend when trying to handle a feature that needs a bound project, e.g. open hotspot.
    * @return the response to this binding assist request, that contains the bound project. The client can cancel the request if the user stops the binding process.
-   * When cancelling the request from the client side, the error code should be {@link org.eclipse.lsp4j.jsonrpc.messages.ResponseErrorCode#ServerCancelled}
+   * @throws java.util.concurrent.CancellationException if the client cancels the process
    */
-  @JsonRequest
-  CompletableFuture<AssistBindingResponse> assistBinding(AssistBindingParams params);
+  default AssistBindingResponse assistBinding(AssistBindingParams params, CancelChecker cancelChecker) {
+    throw new CancellationException("Not implemented");
+  }
 
   /**
    * Requests the client to start showing progress to users.
-   * If there is an error while creating the corresponding UI, clients can fail the returned future.
-   * Tasks requesting the start of the progress should wait for the client to answer before continuing.
+   * @throws org.eclipse.lsp4j.jsonrpc.ResponseErrorException with {@link ClientErrorCode#PROGRESS_CREATION_FAILED} if there is an error while creating the corresponding UI
    */
-  @JsonRequest
-  CompletableFuture<Void> startProgress(StartProgressParams params);
+  default void startProgress(StartProgressParams params, CancelChecker cancelChecker) {
+    throw new ResponseErrorException(new ResponseError(ClientErrorCode.PROGRESS_CREATION_FAILED, "Progress not supported", null));
+  }
 
   /**
    * Reports progress to the client.
    */
-  @JsonNotification
   void reportProgress(ReportProgressParams params);
 
-  @JsonNotification
   void didSynchronizeConfigurationScopes(DidSynchronizeConfigurationScopeParams params);
 
   /**
-   * @throws org.eclipse.lsp4j.jsonrpc.ResponseErrorException with {@link org.sonarsource.sonarlint.core.rpc.protocol.client.ClientErrorCode#CONNECTION_NOT_FOUND} if the connection doesn't exist on the client side
+   * @throws org.eclipse.lsp4j.jsonrpc.ResponseErrorException with {@link ClientErrorCode#CONNECTION_NOT_FOUND} if the connection doesn't exist on the client side
    */
-  @JsonRequest
-  CompletableFuture<GetCredentialsResponse> getCredentials(GetCredentialsParams params);
+  GetCredentialsResponse getCredentials(GetCredentialsParams params, CancelChecker cancelChecker);
 
-  @JsonRequest
-  CompletableFuture<SelectProxiesResponse> selectProxies(SelectProxiesParams params);
+  default SelectProxiesResponse selectProxies(SelectProxiesParams params, CancelChecker cancelChecker) {
+    var proxies = ProxySelector.getDefault().select(URI.create(params.getUri()));
+    return new SelectProxiesResponse(proxies.stream().map(SonarLintRpcClientDelegate::convert).collect(Collectors.toList()));
+  }
+
+  private static ProxyDto convert(Proxy proxy) {
+    if (proxy.type() == Proxy.Type.DIRECT) {
+      return ProxyDto.NO_PROXY;
+    }
+    var address = (InetSocketAddress) proxy.address();
+    var server = address.getHostString();
+    var port = address.getPort();
+    return new ProxyDto(proxy.type(), server, port);
+  }
 
   /**
    * @throws org.eclipse.lsp4j.jsonrpc.ResponseErrorException with {@link ResponseErrorCode#InvalidParams} if the targetHostUrl is not a valid URL
    */
-  @JsonRequest
-  CompletableFuture<GetProxyPasswordAuthenticationResponse> getProxyPasswordAuthentication(GetProxyPasswordAuthenticationParams params);
+  default GetProxyPasswordAuthenticationResponse getProxyPasswordAuthentication(GetProxyPasswordAuthenticationParams params, CancelChecker cancelChecker) {
+    // use null addr, because the authentication fails if it does not exactly match the expected realm's host
+    URL targetHostUrl;
+    try {
+      targetHostUrl = new URL(params.getTargetHostURL());
+    } catch (MalformedURLException e) {
+      throw new ResponseErrorException(new ResponseError(ResponseErrorCode.InvalidParams, "targetHostUrl is not a valid URL: " + params.getTargetHostURL(), null));
+    }
+    var passwordAuthentication = Authenticator.requestPasswordAuthentication(params.getHost(), null, params.getPort(), params.getProtocol(), params.getPrompt(), params.getScheme(),
+      targetHostUrl, Authenticator.RequestorType.PROXY);
+    return new GetProxyPasswordAuthenticationResponse(passwordAuthentication != null ? passwordAuthentication.getUserName() : null,
+      passwordAuthentication != null ? new String(passwordAuthentication.getPassword()) : null);
+  }
 
-  @JsonRequest
-  CompletableFuture<CheckServerTrustedResponse> checkServerTrusted(CheckServerTrustedParams params);
+  default CheckServerTrustedResponse checkServerTrusted(CheckServerTrustedParams params, CancelChecker cancelChecker) {
+    return new CheckServerTrustedResponse(false);
+  }
 
-  @JsonNotification
   void didReceiveServerTaintVulnerabilityRaisedEvent(DidReceiveServerTaintVulnerabilityRaisedEvent params);
 
-  @JsonNotification
   void didReceiveServerTaintVulnerabilityChangedOrClosedEvent(DidReceiveServerTaintVulnerabilityChangedOrClosedEvent params);
 
-  @JsonNotification
   void didReceiveServerHotspotEvent(DidReceiveServerHotspotEvent params);
 
-  @JsonRequest
-  CompletableFuture<MatchSonarProjectBranchResponse> matchSonarProjectBranch(MatchSonarProjectBranchParams params);
+  MatchSonarProjectBranchResponse matchSonarProjectBranch(MatchSonarProjectBranchParams params, CancelChecker cancelChecker);
 
-  @JsonNotification
   void didChangeMatchedSonarProjectBranch(DidChangeMatchedSonarProjectBranchParams params);
 }
