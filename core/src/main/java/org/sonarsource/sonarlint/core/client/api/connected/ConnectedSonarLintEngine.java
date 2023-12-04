01561f272af91517206b64220effb78cd8b6fc55
Vojtech Suchy <vojtech.suchy@sonarsource.com>
2023-12-04 09:27:01 +0100

    SLCORE-640 Replace calculatePathPrefixes(), downloadAllServerIssuesForFile() and syncServerIssues() with RPC services

--- core/src/main/java/org/sonarsource/sonarlint/core/client/api/connected/ConnectedSonarLintEngine.java
+++ core/src/main/java/org/sonarsource/sonarlint/core/client/api/connected/ConnectedSonarLintEngine.java
@@ -19,7 +19,6 @@
  */
 package org.sonarsource.sonarlint.core.client.api.connected;
 
-import java.util.Collection;
 import java.util.List;
 import java.util.Set;
 import javax.annotation.Nullable;
@@ -29,9 +28,7 @@ import org.sonarsource.sonarlint.core.client.api.common.analysis.IssueListener;
 import org.sonarsource.sonarlint.core.commons.log.ClientLogOutput;
 import org.sonarsource.sonarlint.core.commons.progress.ClientProgressMonitor;
 import org.sonarsource.sonarlint.core.http.HttpClient;
-import org.sonarsource.sonarlint.core.rpc.protocol.backend.file.GetPathTranslationParams;
 import org.sonarsource.sonarlint.core.serverapi.EndpointParams;
-import org.sonarsource.sonarlint.core.serverconnection.DownloadException;
 import org.sonarsource.sonarlint.core.serverconnection.ProjectBinding;
 import org.sonarsource.sonarlint.core.serverconnection.issues.ServerIssue;
 
@@ -58,18 +55,6 @@ public interface ConnectedSonarLintEngine extends SonarLintEngine {
   @Deprecated(since = "9.3")
   List<ServerIssue> getServerIssues(ProjectBinding projectBinding, String branchName, String filePath);
 
-  /**
-   * Tries to find the best way to match files in a IDE project with files in the sonarqube project identified
-   * with {@code projectKey}, by finding file path prefixes to be used later in other interactions with the project storage.
-   * Requires the storage of the project to be up to date.
-   *
-   * @param ideFilePaths List of relative paths of all file belonging to the project in the IDE
-   * @since 3.10
-   * @deprecated use {@link org.sonarsource.sonarlint.core.rpc.protocol.backend.file.FileRpcService#getPathTranslation(GetPathTranslationParams)}
-   */
-  @Deprecated(since = "10.0")
-  ProjectBinding calculatePathPrefixes(String projectKey, Collection<String> ideFilePaths);
-
   // REQUIRES SERVER TO BE REACHABLE
 
   /**
@@ -88,31 +73,10 @@ public interface ConnectedSonarLintEngine extends SonarLintEngine {
   void updateProject(EndpointParams endpoint, HttpClient client, String projectKey, @Nullable ClientProgressMonitor monitor);
 
   /**
-   * Downloads and stores server issues for a given file. Starting from SQ 9.6, this is noop as issues updates are coming by SSE.
-   *
-   * @param projectBinding information about the project (must have been previously updated with {@link #updateProject(EndpointParams, HttpClient, String, ClientProgressMonitor)})
-   * @param ideFilePath    relative to the project in the IDE.
-   * @throws DownloadException if it fails to download
-   * @deprecated synchronization is managed by the new backend
-   */
-  @Deprecated(since = "9.3")
-  void downloadAllServerIssuesForFile(EndpointParams endpoint, HttpClient client, ProjectBinding projectBinding, String ideFilePath, String branchName,
-    @Nullable ClientProgressMonitor monitor);
-
-  /**
    * Downloads and stores all server issues for a given project.
    *
    * @param endpoint from which to download issues
    * @param projectKey   key of the project (must have been previously updated with {@link #updateProject(EndpointParams, HttpClient, String, ClientProgressMonitor)})
    */
   void downloadAllServerIssues(EndpointParams endpoint, HttpClient client, String projectKey, String branchName, @Nullable ClientProgressMonitor monitor);
-
-  /**
-   * Sync server issues incrementally for a given project (will only work for supported servers).
-   *
-   * @param endpoint from which to download issues
-   * @param projectKey   key of the project
-   */
-  @Deprecated(since = "10.0")
-  void syncServerIssues(EndpointParams endpoint, HttpClient client, String projectKey, String branchName, @Nullable ClientProgressMonitor monitor);
 }
