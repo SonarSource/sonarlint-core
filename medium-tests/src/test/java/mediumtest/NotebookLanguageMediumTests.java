6214e756b1166cbd234abdcd7d1e1b7af2df8621
Vojtech Suchy <vojtech.suchy@sonarsource.com>
2023-12-05 11:16:03 +0100

    SLCORE-655 Remove downloadAllServerIssues() and getServerIssues() from SonarLintEngine in favour of RPC

--- medium-tests/src/test/java/mediumtest/NotebookLanguageMediumTests.java
+++ medium-tests/src/test/java/mediumtest/NotebookLanguageMediumTests.java
@@ -19,8 +19,7 @@
  */
 package mediumtest;
 
-import com.github.tomakehurst.wiremock.junit5.WireMockExtension;
-import java.util.List;
+import java.util.Set;
 import java.util.concurrent.ExecutionException;
 import mediumtest.fixtures.SonarLintTestRpcServer;
 import mediumtest.fixtures.TestPlugin;
@@ -29,39 +28,20 @@ import org.junit.jupiter.api.AfterAll;
 import org.junit.jupiter.api.BeforeAll;
 import org.junit.jupiter.api.Test;
 import org.junit.jupiter.api.extension.RegisterExtension;
-import org.sonarsource.sonarlint.core.ConnectedSonarLintEngineImpl;
-import org.sonarsource.sonarlint.core.NodeJsHelper;
-import org.sonarsource.sonarlint.core.analysis.api.ClientModuleFileSystem;
-import org.sonarsource.sonarlint.core.analysis.api.ClientModuleInfo;
-import org.sonarsource.sonarlint.core.client.api.connected.ConnectedGlobalConfiguration;
 import org.sonarsource.sonarlint.core.commons.Language;
 import org.sonarsource.sonarlint.core.commons.log.SonarLintLogTester;
-import org.sonarsource.sonarlint.core.serverapi.EndpointParams;
-import org.sonarsource.sonarlint.core.serverapi.proto.sonarqube.ws.Common;
-import org.sonarsource.sonarlint.core.serverapi.proto.sonarqube.ws.Issues;
+import org.sonarsource.sonarlint.core.serverconnection.ServerConnection;
 
-import static com.github.tomakehurst.wiremock.client.WireMock.aResponse;
-import static com.github.tomakehurst.wiremock.client.WireMock.get;
-import static com.github.tomakehurst.wiremock.core.WireMockConfiguration.wireMockConfig;
 import static mediumtest.fixtures.SonarLintBackendFixture.newBackend;
 import static mediumtest.fixtures.SonarLintBackendFixture.newFakeClient;
-import static org.assertj.core.api.AssertionsForClassTypes.assertThatCode;
-import static org.mockito.Mockito.mock;
-import static testutils.TestUtils.createNoOpLogOutput;
-import static testutils.TestUtils.protobufBodyDelimited;
+import static org.assertj.core.api.Assertions.assertThat;
 
 class NotebookLanguageMediumTests {
   @RegisterExtension
   private static final SonarLintLogTester logTester = new SonarLintLogTester();
 
-  @RegisterExtension
-  static WireMockExtension sonarqubeMock = WireMockExtension.newInstance()
-    .options(wireMockConfig().dynamicPort())
-    .build();
-
   private static final String CONNECTION_ID = StringUtils.repeat("very-long-id", 30);
   private static final String JAVA_MODULE_KEY = "test-project-2";
-  private static ConnectedSonarLintEngineImpl sonarlint;
   private static SonarLintTestRpcServer backend;
 
   @BeforeAll
@@ -69,58 +49,24 @@ class NotebookLanguageMediumTests {
     var fakeClient = newFakeClient()
       .build();
     backend = newBackend()
-      .withSonarQubeConnection(CONNECTION_ID, sonarqubeMock.baseUrl())
       .withStorage(CONNECTION_ID, s -> s.withPlugins(TestPlugin.JAVASCRIPT, TestPlugin.JAVA)
         .withProject("test-project")
         .withProject(JAVA_MODULE_KEY))
       .build(fakeClient);
-
-    var nodeJsHelper = new NodeJsHelper(logTester.getLogOutput());
-    nodeJsHelper.detect(null);
-
-    var config = ConnectedGlobalConfiguration.sonarQubeBuilder()
-      .setConnectionId(CONNECTION_ID)
-      .setSonarLintUserHome(backend.getUserHome())
-      .setStorageRoot(backend.getStorageRoot())
-      .setLogOutput(createNoOpLogOutput())
-      .addEnabledLanguages(Language.JAVA, Language.JS, Language.IPYTHON)
-      .setNodeJs(nodeJsHelper.getNodeJsPath(), nodeJsHelper.getNodeJsVersion())
-      .setModulesProvider(() -> List.of(new ClientModuleInfo("key", mock(ClientModuleFileSystem.class))))
-      .build();
-    sonarlint = new ConnectedSonarLintEngineImpl(config);
   }
 
   @AfterAll
   static void stop() throws ExecutionException, InterruptedException {
-    if (sonarlint != null) {
-      sonarlint.stop();
-      sonarlint = null;
-    }
     if (backend != null) {
       backend.shutdown().get();
     }
   }
 
   @Test
-  void should_not_pull_issues_for_notebook_python_language() {
-    sonarqubeMock.stubFor(get("/api/system/status")
-      .willReturn(aResponse().withStatus(200).withBody("{\"status\": \"UP\", \"version\": \"9.6\", \"id\": \"xzy\"}")));
-    var timestamp = Issues.IssuesPullQueryTimestamp.newBuilder().setQueryTimestamp(123L).build();
-    var issue = Issues.IssueLite.newBuilder()
-      .setKey("uuid")
-      .setRuleKey("sonarjava:S123")
-      .setType(Common.RuleType.BUG)
-      .setMainLocation(Issues.Location.newBuilder().setFilePath("foo/bar/Hello.java").setMessage("Primary message")
-        .setTextRange(Issues.TextRange.newBuilder().setStartLine(1).setStartLineOffset(2).setEndLine(3)
-          .setEndLineOffset(4).setHash("hash")))
-      .setCreationDate(123456789L)
-      .build();
-
-    sonarqubeMock.stubFor(get("/api/issues/pull?projectKey=test-project&branchName=master&languages=java,js")
-      .willReturn(aResponse().withStatus(200).withResponseBody(protobufBodyDelimited(timestamp, issue))));
-
-    assertThatCode(() -> sonarlint.downloadAllServerIssues(new EndpointParams(sonarqubeMock.baseUrl(), false, null),
-      backend.getHttpClient(CONNECTION_ID), "test-project", "master", null)).doesNotThrowAnyException();
+  void should_not_enable_sync_for_notebook_python_language() {
+    var serverConnection = new ServerConnection(backend.getStorageRoot(), CONNECTION_ID, false, Set.of(Language.JAVA, Language.JS,
+      Language.IPYTHON), Set.of(), backend.getWorkDir());
+    assertThat(serverConnection.getEnabledLanguagesToSync()).containsOnly(Language.JAVA, Language.JS);
   }
 
 }
