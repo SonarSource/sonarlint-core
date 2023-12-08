3b10f8d1a7f781dade625cafd9e00c603881fa9b
Julien HENRY <julien.henry@sonarsource.com>
2023-12-08 16:15:06 +0100

    Move ThreadFactories to core

--- core/src/main/java/org/sonarsource/sonarlint/core/commons/ThreadFactories.java
+++ core/src/main/java/org/sonarsource/sonarlint/core/commons/ThreadFactories.java
@@ -1,5 +1,5 @@
 /*
- * SonarLint Core - Commons
+ * SonarLint Core - Implementation
  * Copyright (C) 2016-2023 SonarSource SA
  * mailto:info AT sonarsource DOT com
  *
@@ -17,7 +17,7 @@
  * along with this program; if not, write to the Free Software Foundation,
  * Inc., 51 Franklin Street, Fifth Floor, Boston, MA  02110-1301, USA.
  */
-package org.sonarsource.sonarlint.core.commons.concurrent;
+package org.sonarsource.sonarlint.core.commons;
 
 import java.util.concurrent.ThreadFactory;
 import java.util.concurrent.atomic.AtomicInteger;
