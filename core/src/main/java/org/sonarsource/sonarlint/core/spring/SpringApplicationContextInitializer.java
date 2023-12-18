aa0f078b2e127dba6555e0d21002e8cb86d9d19a
Julien HENRY <julien.henry@sonarsource.com>
2023-12-18 13:35:43 +0100

    Move SpringApplicationContextInitializer

--- core/src/main/java/org/sonarsource/sonarlint/core/spring/SpringApplicationContextInitializer.java
+++ core/src/main/java/org/sonarsource/sonarlint/core/spring/SpringApplicationContextInitializer.java
@@ -17,7 +17,7 @@
  * along with this program; if not, write to the Free Software Foundation,
  * Inc., 51 Franklin Street, Fifth Floor, Boston, MA  02110-1301, USA.
  */
-package org.sonarsource.sonarlint.core;
+package org.sonarsource.sonarlint.core.spring;
 
 import org.sonarsource.sonarlint.core.rpc.protocol.SonarLintRpcClient;
 import org.sonarsource.sonarlint.core.rpc.protocol.backend.initialize.InitializeParams;
