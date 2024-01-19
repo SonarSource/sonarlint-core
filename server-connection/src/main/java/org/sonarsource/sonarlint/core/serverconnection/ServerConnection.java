fe95c602c8e5a2b6d105f4d08ad6e1337f5ee3a9
Jean-Baptiste Lievremont <jeanbaptiste.lievremont@sonarsource.com>
2024-01-19 16:30:40 +0100

    SLCORE-668 Remove unused import

--- server-connection/src/main/java/org/sonarsource/sonarlint/core/serverconnection/ServerConnection.java
+++ server-connection/src/main/java/org/sonarsource/sonarlint/core/serverconnection/ServerConnection.java
@@ -28,7 +28,6 @@ import java.util.Map;
 import java.util.Set;
 import java.util.function.Consumer;
 import java.util.function.IntPredicate;
-import java.util.function.Predicate;
 import java.util.stream.Collectors;
 import org.apache.commons.io.FilenameUtils;
 import org.sonarsource.sonarlint.core.commons.Language;
