1977242e81bc7bd4ec618f33e091494facfd84d7
Julien HENRY <julien.henry@sonarsource.com>
2024-02-01 12:25:07 +0100

    SLCORE-685 Disable tests

--- backend/server-connection/src/test/java/org/sonarsource/sonarlint/core/serverconnection/VersionUtilsTests.java
+++ backend/server-connection/src/test/java/org/sonarsource/sonarlint/core/serverconnection/VersionUtilsTests.java
@@ -19,6 +19,7 @@
  */
 package org.sonarsource.sonarlint.core.serverconnection;
 
+import org.junit.jupiter.api.Disabled;
 import org.junit.jupiter.api.Test;
 import org.sonarsource.sonarlint.core.commons.Version;
 
@@ -48,6 +49,7 @@ class VersionUtilsTests {
   }
 
   @Test
+  @Disabled("SLCORE-685 This test fails depending on the current date")
   void grace_period_should_be_true_if_connected_during_grace_period() {
     assertThat(VersionUtils.isVersionSupportedDuringGracePeriod(getMinimalSupportedVersion())).isTrue();
     assertThat(VersionUtils.isVersionSupportedDuringGracePeriod(Version.create(getMinimalSupportedVersion().getName() + ".1"))).isTrue();
