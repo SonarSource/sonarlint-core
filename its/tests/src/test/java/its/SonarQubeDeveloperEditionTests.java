52e55d047505ed1f24248b37c27759dba6d241cb
Damien Urruty <damien.urruty@sonarsource.com>
2024-02-05 19:21:11 +0100

    Fix failing test consecutive to analyzer metadata update
    The rule description of javasecurity:S2083 was updated in SQ 10.4

--- its/tests/src/test/java/its/SonarQubeDeveloperEditionTests.java
+++ its/tests/src/test/java/its/SonarQubeDeveloperEditionTests.java
@@ -1299,12 +1299,14 @@ class SonarQubeDeveloperEditionTests extends AbstractConnectedTests {
 
       var description = activeRuleDetailsResponse.details().getDescription();
 
-      if (!ORCHESTRATOR.getServer().version().isGreaterThanOrEquals(9, 5)) {
+      var serverVersion = ORCHESTRATOR.getServer().version();
+      if (!serverVersion.isGreaterThanOrEquals(9, 5)) {
         // no description sections at that time
         assertThat(description.isRight()).isFalse();
       } else {
         var extendedDescription = description.getRight();
         assertThat(extendedDescription.getIntroductionHtmlContent()).isNull();
+        var link = serverVersion.isGreaterThanOrEquals(10, 4) ? "OWASP - <a href=..." : "<a href=\"https:/...";
         assertThat(extendedDescription.getTabs())
           .flatExtracting(this::extractTabContent)
           .containsExactly(
@@ -1318,7 +1320,7 @@ class SonarQubeDeveloperEditionTests extends AbstractConnectedTests {
             "More Info",
             "<h3>Standards</h3>\n"
               + "<ul>\n"
-              + "  <li> <a href=\"https:/...");
+              + "  <li> " + link);
 
         var howToFixTab = extendedDescription.getTabs().get(1);
         assertThat(howToFixTab.getContent().getRight().getDefaultContextKey()).isEqualTo("others");
