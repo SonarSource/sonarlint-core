d27c45546492c2c056f7c3dd0090fdb5634d03c6
Julien HENRY <julien.henry@sonarsource.com>
2024-02-06 13:49:02 +0100

    Clean shell scripts

--- backend/cli/src/main/assembly/bin/sonarlint-backend.bat
+++ backend/cli/src/main/assembly/bin/sonarlint-backend.bat
@@ -1,39 +1,14 @@
-@rem
-@rem Copyright 2015 the original author or authors.
-@rem
-@rem Licensed under the Apache License, Version 2.0 (the "License");
-@rem you may not use this file except in compliance with the License.
-@rem You may obtain a copy of the License at
-@rem
-@rem      https://www.apache.org/licenses/LICENSE-2.0
-@rem
-@rem Unless required by applicable law or agreed to in writing, software
-@rem distributed under the License is distributed on an "AS IS" BASIS,
-@rem WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
-@rem See the License for the specific language governing permissions and
-@rem limitations under the License.
-@rem
-
 @if "%DEBUG%"=="" @echo off
-@rem ##########################################################################
-@rem
-@rem  SLCORE startup script for Windows
-@rem
-@rem ##########################################################################
 
 @rem Set local scope for the variables with windows NT shell
 if "%OS%"=="Windows_NT" setlocal
 
 set DIRNAME=%~dp0
 if "%DIRNAME%"=="" set DIRNAME=.
-@rem This is normally unused
-set APP_BASE_NAME=%~n0
 set APP_HOME=%DIRNAME%..
 
 @rem Resolve any "." and ".." in APP_HOME to make it shorter.
 for %%i in ("%APP_HOME%") do set APP_HOME=%%~fi
-
-set args=
 
 :loop
 IF NOT "%1"=="" (
@@ -59,8 +34,7 @@ set CLASSPATH=%APP_HOME%\lib\*;
 "%JAVA_EXE%" ^
    %SONARLINT_JVM_OPTS% ^
    -classpath "%CLASSPATH%" ^
-   org.sonarsource.sonarlint.core.backend.cli.SonarLintServerCli ^
-   %args%
+   org.sonarsource.sonarlint.core.backend.cli.SonarLintServerCli
 
 @rem End local scope for the variables with windows NT shell
 if %ERRORLEVEL% equ 0 goto mainEnd
