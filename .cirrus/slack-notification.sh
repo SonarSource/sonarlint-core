1e84885dbdd9b01857997e94abba9cfa5ff1a03b
Tobi Hahnen <tobias.hahnen@sonarsource.com>
2024-03-14 12:12:51 +0100

    Intendation breaking Bash

--- .cirrus/slack-notification.sh
+++ .cirrus/slack-notification.sh
@@ -7,24 +7,24 @@ if [[ $CIRRUS_PR == "" ]]; then
 	-H "Authorization: Bearer ${SLACK_TOKEN}" \
 	-H 'Content-type: application/json; charset=utf-8' \
 	--data-binary @- <<EOF
-	{
-	"channel": "squad-sonar-solution-bots",
-		"blocks": [
-			{
-				"type": "header",
-				"text": {
-					"type": "plain_text",
-					"text": "Cirrus CI build failure on pipeline"
-				}
-			},
-			{
-				"type": "section",
-				"text": {
-					"type": "mrkdwn",
-					"text": "Task *<https://cirrus-ci.com/task/$CIRRUS_TASK_ID|$CIRRUS_TASK_NAME>* failed on *<$CIRRUS_REPO_CLONE_URL|$CIRRUS_REPO_FULL_NAME>* (*$CIRRUS_BRANCH*)"
-				}
+{
+"channel": "squad-sonar-solution-bots",
+	"blocks": [
+		{
+			"type": "header",
+			"text": {
+				"type": "plain_text",
+				"text": "Cirrus CI build failure on pipeline"
 			}
-		]
-	}
-	EOF
+		},
+		{
+			"type": "section",
+			"text": {
+				"type": "mrkdwn",
+				"text": "Task *<https://cirrus-ci.com/task/$CIRRUS_TASK_ID|$CIRRUS_TASK_NAME>* failed on *<$CIRRUS_REPO_CLONE_URL|$CIRRUS_REPO_FULL_NAME>* (*$CIRRUS_BRANCH*)"
+			}
+		}
+	]
+}
+EOF
 fi
