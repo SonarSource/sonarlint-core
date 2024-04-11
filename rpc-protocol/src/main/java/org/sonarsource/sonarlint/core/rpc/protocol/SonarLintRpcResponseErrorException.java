853493f388a7f87993a6923dbfa75b5f651ee424
Vojtech Suchy <vojtech.suchy@sonarsource.com>
2024-04-11 13:54:20 +0200

    SLCORE-663 Stop leaking ResponseError and ResponseErrorException type

---
+++ rpc-protocol/src/main/java/org/sonarsource/sonarlint/core/rpc/protocol/SonarLintRpcResponseErrorException.java
@@ -0,0 +1,44 @@
+/*
+ * SonarLint Core - RPC Protocol
+ * Copyright (C) 2016-2024 SonarSource SA
+ * mailto:info AT sonarsource DOT com
+ *
+ * This program is free software; you can redistribute it and/or
+ * modify it under the terms of the GNU Lesser General Public
+ * License as published by the Free Software Foundation; either
+ * version 3 of the License, or (at your option) any later version.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the GNU
+ * Lesser General Public License for more details.
+ *
+ * You should have received a copy of the GNU Lesser General Public License
+ * along with this program; if not, write to the Free Software Foundation,
+ * Inc., 51 Franklin Street, Fifth Floor, Boston, MA  02110-1301, USA.
+ */
+package org.sonarsource.sonarlint.core.rpc.protocol;
+
+/*
+ * A class to use in place of {@link org.eclipse.lsp4j.jsonrpc.messages.ResponseError.ResponseErrorException} to stop depending on lsp4j
+ * types in API and services.
+ * See SLCORE-663 for details.
+ */
+public class SonarLintRpcResponseErrorException extends RuntimeException {
+
+  private static final long serialVersionUID = -5970739895395246885L;
+  private final SonarLintRpcResponseError responseError;
+
+  public SonarLintRpcResponseErrorException(SonarLintRpcResponseError responseError) {
+    this.responseError = responseError;
+  }
+
+  @Override
+  public String getMessage() {
+    return responseError.getMessage();
+  }
+
+  public SonarLintRpcResponseError getResponseError() {
+    return responseError;
+  }
+}
