963a3dd7b886f0f37015ac87ef9b93df9f60a6d8
Damien Urruty <damien.urruty@sonarsource.com>
2024-04-17 15:29:54 +0200

    SLCORE-710 Let clients provide a unique analysis ID

--- rpc-protocol/src/main/java/org/sonarsource/sonarlint/core/rpc/protocol/backend/analysis/AnalyzeFilesParams.java
+++ rpc-protocol/src/main/java/org/sonarsource/sonarlint/core/rpc/protocol/backend/analysis/AnalyzeFilesParams.java
@@ -22,16 +22,20 @@ package org.sonarsource.sonarlint.core.rpc.protocol.backend.analysis;
 import java.net.URI;
 import java.util.List;
 import java.util.Map;
+import java.util.UUID;
 
 public class AnalyzeFilesParams {
   private final String configurationScopeId;
+  // this is a random id provided by the client that will be used to correlate the raw issues notified back to the client
+  private final UUID analysisId;
   private final List<URI> filesToAnalyze;
   private final Map<String, String> extraProperties;
   // this is determined by the client as other operations could occur before reaching the backend
   private final long startTime;
 
-  public AnalyzeFilesParams(String configurationScopeId, List<URI> filesToAnalyze, Map<String, String> extraProperties, long startTime) {
+  public AnalyzeFilesParams(String configurationScopeId, UUID analysisId, List<URI> filesToAnalyze, Map<String, String> extraProperties, long startTime) {
     this.configurationScopeId = configurationScopeId;
+    this.analysisId = analysisId;
     this.filesToAnalyze = filesToAnalyze;
     this.extraProperties = extraProperties;
     this.startTime = startTime;
@@ -39,6 +43,10 @@ public class AnalyzeFilesParams {
 
   public String getConfigurationScopeId() {
     return configurationScopeId;
+  }
+
+  public UUID getAnalysisId() {
+    return analysisId;
   }
 
   public List<URI> getFilesToAnalyze() {
