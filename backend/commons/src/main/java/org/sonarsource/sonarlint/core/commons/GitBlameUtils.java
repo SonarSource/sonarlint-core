4540aca359832265f1a3f1a353694fb688231141
Serhat Yenican <serhat.yenican@sonarsource.com>
2024-05-06 09:55:04 +0200

    SLCORE-768 provide git blaming with base dir path

--- backend/commons/src/main/java/org/sonarsource/sonarlint/core/commons/GitBlameUtils.java
+++ backend/commons/src/main/java/org/sonarsource/sonarlint/core/commons/GitBlameUtils.java
@@ -19,18 +19,50 @@
  */
 package org.sonarsource.sonarlint.core.commons;
 
+import java.io.IOException;
 import java.nio.file.Path;
 import java.util.Set;
 import java.util.stream.Collectors;
 import org.eclipse.jgit.api.errors.GitAPIException;
 import org.eclipse.jgit.diff.RawTextComparator;
+import org.eclipse.jgit.lib.ObjectReader;
 import org.eclipse.jgit.lib.Repository;
+import org.eclipse.jgit.lib.RepositoryBuilder;
 import org.sonar.scm.git.blame.RepositoryBlameCommand;
 
 public class GitBlameUtils {
 
   private GitBlameUtils() {
     // Utility class
+  }
+
+  public static SonarLintBlameResult blameWithFilesGitCommand(Path baseDir, Set<Path> gitRelativePath) {
+    var repo = buildRepository(baseDir);
+    return blameWithFilesGitCommand(repo, gitRelativePath);
+  }
+
+  static Repository buildRepository(Path basedir) {
+    try {
+      var repo = getVerifiedRepositoryBuilder(basedir).build();
+      try (ObjectReader objReader = repo.getObjectDatabase().newReader()) {
+        // SONARSCGIT-2 Force initialization of shallow commits to avoid later concurrent modification issue
+        objReader.getShallowCommits();
+        return repo;
+      }
+    } catch (IOException e) {
+      throw new IllegalStateException("Unable to open Git repository", e);
+    }
+  }
+
+  static RepositoryBuilder getVerifiedRepositoryBuilder(Path basedir) {
+    RepositoryBuilder builder = new RepositoryBuilder()
+      .findGitDir(basedir.toFile())
+      .setMustExist(true);
+
+    if (builder.getGitDir() == null) {
+      throw new IllegalStateException("Not inside a Git work tree: " + basedir);
+    }
+    return builder;
   }
 
   public static SonarLintBlameResult blameWithFilesGitCommand(Repository repo, Set<Path> gitRelativePath) {
