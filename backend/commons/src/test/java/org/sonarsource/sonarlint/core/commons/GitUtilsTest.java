b2e718e8d781ecffb9c8dcb7941f9eff32506ca9
Vojtech Suchy <vojtech.suchy@sonarsource.com>
2024-06-12 14:33:28 +0200

    SLCORE-835 Add license headers; Improve test naming

--- backend/commons/src/test/java/org/sonarsource/sonarlint/core/commons/GitUtilsTest.java
+++ backend/commons/src/test/java/org/sonarsource/sonarlint/core/commons/GitUtilsTest.java
@@ -1,3 +1,22 @@
+/*
+ * SonarLint Core - Commons
+ * Copyright (C) 2016-2024 SonarSource SA
+ * mailto:info AT sonarsource DOT com
+ *
+ * This program is free software; you can redistribute it and/or
+ * modify it under the terms of the GNU Lesser General Public
+ * License as published by the Free Software Foundation; either
+ * version 3 of the License, or (at your option) any later version.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the GNU
+ * Lesser General Public License for more details.
+ *
+ * You should have received a copy of the GNU Lesser General Public License
+ * along with this program; if not, write to the Free Software Foundation,
+ * Inc., 51 Franklin Street, Fifth Floor, Boston, MA  02110-1301, USA.
+ */
 package org.sonarsource.sonarlint.core.commons;
 
 import java.io.IOException;
@@ -63,7 +82,7 @@ class GitUtilsTest {
   }
 
   @Test
-  void should_log_exception_filter_ignored_files() {
+  void should_consider_all_files_not_ignored_on_exception() {
     FileUtils.deleteQuietly(gitDirPath.toFile());
 
     var filteredOutIgnoredFiles = GitUtils.filterOutIgnoredFiles(gitDirPath, List.of(URI.create("fileA"), URI.create("fileB"),
