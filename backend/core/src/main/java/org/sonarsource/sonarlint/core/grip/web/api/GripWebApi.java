044e7d869d0ce39444637184d4bdecc30ba75a48
Damien Urruty <damien.urruty@sonarsource.com>
2024-06-19 10:58:17 +0200

    Improve error handling

--- backend/core/src/main/java/org/sonarsource/sonarlint/core/grip/web/api/GripWebApi.java
+++ backend/core/src/main/java/org/sonarsource/sonarlint/core/grip/web/api/GripWebApi.java
@@ -32,6 +32,7 @@ import org.sonarsource.sonarlint.core.grip.web.api.payload.SuggestFixResponsePay
 import org.sonarsource.sonarlint.core.http.HttpClientProvider;
 import org.sonarsource.sonarlint.core.rpc.protocol.backend.grip.ProvideFeedbackParams;
 import org.sonarsource.sonarlint.core.rpc.protocol.backend.grip.SuggestFixParams;
+import org.sonarsource.sonarlint.core.rpc.protocol.common.Either;
 
 public class GripWebApi {
   private static final SonarLintLogger LOG = SonarLintLogger.get();
@@ -41,14 +42,31 @@ public class GripWebApi {
     this.httpClientProvider = httpClientProvider;
   }
 
-  public SuggestFixResponse suggestFix(SuggestFixParams params, String fileContent) {
+  public Either<String, SuggestFixWebApiResponse> suggestFix(SuggestFixParams params, String fileContent) {
     var requestBody = serializeFixRequestBody(params, fileContent);
     LOG.info("Requesting suggestion from GRIP service: {}", requestBody);
     try (var response = httpClientProvider.getHttpClientWithPreemptiveAuth(params.getAuthenticationToken()).postWithBearer(
       ensureTrailingSlash(params.getServiceURI()) + "api/suggest/", "application/json",
       requestBody)) {
-      var responseBody = deserializeResponseBody(response.bodyAsString());
-      return new SuggestFixResponse(UUID.fromString(response.header("X-Correlation-Id")), responseBody.choices.get(responseBody.choices.size() - 1).message.content);
+      var code = response.code();
+      var responseBodyString = response.bodyAsString();
+      if (code >= 200 && response.code() < 400) {
+        try {
+          LOG.info("Received response from the GRIP service: {}", responseBodyString);
+          var responseBody = deserializeResponseBody(responseBodyString);
+          return Either.forRight(
+            new SuggestFixWebApiResponse(UUID.fromString(response.header("X-Correlation-Id")), responseBody.choices.get(responseBody.choices.size() - 1).message.content));
+        } catch (Exception e) {
+          LOG.error("Unexpected response received from the suggestion service", e);
+          return Either.forLeft("Unexpected response received from the suggestion service");
+        }
+      } else {
+        LOG.info("Error response received: code={}, body={}", code, responseBodyString);
+        return Either.forLeft("An error occurred when requesting the suggestion. See logs for more details");
+      }
+    } catch (Exception e) {
+      LOG.error("Error requesting suggestions from the GRIP service", e);
+      return Either.forLeft("Error requesting suggestions from the GRIP service. See logs for more details");
     }
   }
 
