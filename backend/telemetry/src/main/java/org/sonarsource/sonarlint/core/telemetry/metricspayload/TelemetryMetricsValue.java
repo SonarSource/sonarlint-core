570a6ba2ce4f57715d5e193f807937627de9a43c
Jean-Baptiste Lievremont <jeanbaptiste.lievremont@sonarsource.com>
2024-09-06 10:46:43 +0200

    SLCORE-938 Implement telemetry metrics system for shared connected mode settings

---
+++ backend/telemetry/src/main/java/org/sonarsource/sonarlint/core/telemetry/metricspayload/TelemetryMetricsValue.java
@@ -0,0 +1,44 @@
+/*
+ * SonarLint Core - Telemetry
+ * Copyright (C) 2016-2024 SonarSource SA
+ * mailto:info AT sonarsource DOT com
+ *
+ * This program is free software; you can redistribute it and/or
+ * modify it under the terms of the GNU Lesser General Public
+ * License as published by the Free Software Foundation; either
+ * version 3 of the License, or (at your option) any later version.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the GNU
+ * Lesser General Public License for more details.
+ *
+ * You should have received a copy of the GNU Lesser General Public License
+ * along with this program; if not, write to the Free Software Foundation,
+ * Inc., 51 Franklin Street, Fifth Floor, Boston, MA  02110-1301, USA.
+ */
+package org.sonarsource.sonarlint.core.telemetry.metricspayload;
+
+import com.google.gson.annotations.SerializedName;
+
+public class TelemetryMetricsValue {
+
+  @SerializedName("key")
+  private final String key;
+
+  @SerializedName("value")
+  private final String value;
+
+  @SerializedName("type")
+  private final TelemetryMetricsValueType type;
+
+  @SerializedName("granularity")
+  private final TelemetryMetricsValueGranularity granularity;
+
+  public TelemetryMetricsValue(String key, String value, TelemetryMetricsValueType type, TelemetryMetricsValueGranularity granularity) {
+    this.key = key;
+    this.value = value;
+    this.type = type;
+    this.granularity = granularity;
+  }
+}
