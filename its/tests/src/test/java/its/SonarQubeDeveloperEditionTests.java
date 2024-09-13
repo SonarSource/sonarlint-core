30e49694ec7c3a22a991e53c8246c64d3c23972f
Kirill Knize <kirill.knize@sonarsource.com>
2024-09-13 10:12:12 +0200

    SLCORE-914 Remove deprecated constructors for InitializeParams

--- its/tests/src/test/java/its/SonarQubeDeveloperEditionTests.java
+++ its/tests/src/test/java/its/SonarQubeDeveloperEditionTests.java
@@ -215,13 +215,13 @@ class SonarQubeDeveloperEditionTests extends AbstractConnectedTests {
           new InitializeParams(IT_CLIENT_INFO, IT_TELEMETRY_ATTRIBUTES, HttpConfigurationDto.defaultConfig(), null, featureFlags,
             sonarUserHome.resolve("storage"),
             sonarUserHome.resolve("work"),
-            Collections.emptySet(), PluginLocator.getEmbeddedPluginsByKeyForTests(),
-            languages, Collections.emptySet(),
+            emptySet(), PluginLocator.getEmbeddedPluginsByKeyForTests(),
+            languages, emptySet(), emptySet(),
             List.of(new SonarQubeConnectionConfigurationDto(CONNECTION_ID, ORCHESTRATOR.getServer().getUrl(), false),
               new SonarQubeConnectionConfigurationDto(CONNECTION_ID_WRONG_CREDENTIALS, ORCHESTRATOR.getServer().getUrl(), false)),
-            Collections.emptyList(),
+            emptyList(),
             sonarUserHome.toString(),
-            Map.of(), false, null))
+            Map.of(), false, null, false, null))
         .get();
     } catch (Exception e) {
       throw new IllegalStateException("Cannot initialize the backend", e);
