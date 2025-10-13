0829bd11d4be5a556be0c6381b8d36539edcf1cc
Kirill Knize <kirill.knize@sonarsource.com>
2025-10-13 16:25:05 +0200

    SLCORE-1762 Migrate Xodus known findings store to H2 DB

---
+++ backend/commons/src/main/java/org/sonarsource/sonarlint/core/commons/storage/repository/KnownFindingsRepository.java
@@ -0,0 +1,150 @@
+/*
+ * SonarLint Core - Commons
+ * Copyright (C) 2016-2025 SonarSource SA
+ * mailto:info AT sonarsource DOT com
+ *
+ * This program is free software; you can redistribute it and/or
+ * modify it under the terms of the GNU Lesser General Public
+ * License as published by the Free Software Foundation; either
+ * version 3 of the License, or (at your option) any later version.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the GNU
+ * Lesser General Public License for more details.
+ *
+ * You should have received a copy of the GNU Lesser General Public License
+ * along with this program; if not, write to the Free Software Foundation,
+ * Inc., 51 Franklin Street, Fifth Floor, Boston, MA  02110-1301, USA.
+ */
+package org.sonarsource.sonarlint.core.commons.storage.repository;
+
+import java.nio.file.Path;
+import java.time.LocalDateTime;
+import java.time.ZoneId;
+import java.util.List;
+import java.util.UUID;
+import org.jooq.Configuration;
+import org.jooq.Record;
+import org.sonarsource.sonarlint.core.commons.KnownFinding;
+import org.sonarsource.sonarlint.core.commons.KnownFindingType;
+import org.sonarsource.sonarlint.core.commons.LineWithHash;
+import org.sonarsource.sonarlint.core.commons.api.TextRangeWithHash;
+import org.sonarsource.sonarlint.core.commons.storage.SonarLintDatabase;
+
+import static org.sonarsource.sonarlint.core.commons.storage.model.Tables.KNOWN_FINDINGS;
+
+public class KnownFindingsRepository {
+
+  private final SonarLintDatabase database;
+
+  public KnownFindingsRepository(SonarLintDatabase database) {
+    this.database = database;
+  }
+
+  public void storeKnownIssues(String configurationScopeId, Path clientRelativePath, List<KnownFinding> newKnownIssues) {
+    storeKnownFindings(configurationScopeId, clientRelativePath, newKnownIssues, KnownFindingType.ISSUE);
+  }
+
+  public void storeKnownSecurityHotspots(String configurationScopeId, Path clientRelativePath, List<KnownFinding> newKnownSecurityHotspots) {
+    storeKnownFindings(configurationScopeId, clientRelativePath, newKnownSecurityHotspots, KnownFindingType.HOTSPOT);
+  }
+
+  public List<KnownFinding> loadSecurityHotspotsForFile(String configurationScopeId, Path filePath) {
+    return getKnownFindingsForFile(configurationScopeId, filePath, KnownFindingType.HOTSPOT);
+  }
+
+  public List<KnownFinding> loadIssuesForFile(String configurationScopeId, Path filePath) {
+    return getKnownFindingsForFile(configurationScopeId, filePath, KnownFindingType.ISSUE);
+  }
+
+  private void storeKnownFindings(String configurationScopeId, Path clientRelativePath, List<KnownFinding> newKnownFindings, KnownFindingType type) {
+    database.dsl().transaction((Configuration trx) -> newKnownFindings.forEach(finding -> {
+      var textRangeWithHash = finding.getTextRangeWithHash();
+      var startLine = textRangeWithHash == null ? null : textRangeWithHash.getStartLine();
+      var startLineOffset = textRangeWithHash == null ? null : textRangeWithHash.getStartLineOffset();
+      var endLine = textRangeWithHash == null ? null : textRangeWithHash.getEndLine();
+      var endLineOffset = textRangeWithHash == null ? null : textRangeWithHash.getEndLineOffset();
+      var textRangeHash = textRangeWithHash == null ? null : textRangeWithHash.getHash();
+
+      var lineWithHash = finding.getLineWithHash();
+      var line = lineWithHash == null ? null : lineWithHash.getNumber();
+      var lineHash = lineWithHash == null ? null : lineWithHash.getHash();
+      var introDate = LocalDateTime.ofInstant(finding.getIntroductionDate(), ZoneId.systemDefault());
+      trx.dsl().mergeInto(KNOWN_FINDINGS)
+        .using(trx.dsl().selectOne())
+        .on(KNOWN_FINDINGS.ID.eq(finding.getId().toString()))
+        .whenMatchedThenUpdate()
+        .set(KNOWN_FINDINGS.CONFIGURATION_SCOPE_ID, configurationScopeId)
+        .set(KNOWN_FINDINGS.IDE_RELATIVE_FILE_PATH, clientRelativePath.toString())
+        .set(KNOWN_FINDINGS.SERVER_KEY, finding.getServerKey())
+        .set(KNOWN_FINDINGS.RULE_KEY, finding.getRuleKey())
+        .set(KNOWN_FINDINGS.MESSAGE, finding.getMessage())
+        .set(KNOWN_FINDINGS.INTRODUCTION_DATE, introDate)
+        .set(KNOWN_FINDINGS.FINDING_TYPE, type.name())
+        .set(KNOWN_FINDINGS.START_LINE, startLine)
+        .set(KNOWN_FINDINGS.START_LINE_OFFSET, startLineOffset)
+        .set(KNOWN_FINDINGS.END_LINE, endLine)
+        .set(KNOWN_FINDINGS.END_LINE_OFFSET, endLineOffset)
+        .set(KNOWN_FINDINGS.TEXT_RANGE_HASH, textRangeHash)
+        .set(KNOWN_FINDINGS.LINE, line)
+        .set(KNOWN_FINDINGS.LINE_HASH, lineHash)
+        .whenNotMatchedThenInsert(KNOWN_FINDINGS.ID, KNOWN_FINDINGS.CONFIGURATION_SCOPE_ID, KNOWN_FINDINGS.IDE_RELATIVE_FILE_PATH, KNOWN_FINDINGS.SERVER_KEY, KNOWN_FINDINGS.RULE_KEY,
+          KNOWN_FINDINGS.MESSAGE, KNOWN_FINDINGS.INTRODUCTION_DATE, KNOWN_FINDINGS.FINDING_TYPE,
+          KNOWN_FINDINGS.START_LINE, KNOWN_FINDINGS.START_LINE_OFFSET, KNOWN_FINDINGS.END_LINE, KNOWN_FINDINGS.END_LINE_OFFSET, KNOWN_FINDINGS.TEXT_RANGE_HASH,
+          KNOWN_FINDINGS.LINE, KNOWN_FINDINGS.LINE_HASH)
+        .values(finding.getId().toString(), configurationScopeId, clientRelativePath.toString(), finding.getServerKey(), finding.getRuleKey(),
+          finding.getMessage(), introDate, type.name(),
+          startLine, startLineOffset, endLine, endLineOffset, textRangeHash,
+          line, lineHash
+        )
+        .execute();
+    }));
+  }
+
+  private List<KnownFinding> getKnownFindingsForFile(String configurationScopeId, Path filePath, KnownFindingType type) {
+    var issuesInFile = database.dsl()
+      .selectFrom(KNOWN_FINDINGS)
+      .where(KNOWN_FINDINGS.CONFIGURATION_SCOPE_ID.eq(configurationScopeId)
+        .and(KNOWN_FINDINGS.IDE_RELATIVE_FILE_PATH.eq(filePath.toString()))
+        .and(KNOWN_FINDINGS.FINDING_TYPE.eq(type.name()))
+      )
+      .fetch();
+    return issuesInFile.stream()
+      .map(KnownFindingsRepository::recordToKnownFinding)
+      .toList();
+  }
+
+  private static KnownFinding recordToKnownFinding(Record rec) {
+    var id = UUID.fromString(rec.get(KNOWN_FINDINGS.ID));
+    var introductionDate = rec.get(KNOWN_FINDINGS.INTRODUCTION_DATE).atZone(ZoneId.systemDefault()).toInstant();
+    var textRangeWithHash = getTextRangeWithHash(rec);
+    var lineWithHash = getLineWithHash(rec);
+    return new KnownFinding(
+      id,
+      rec.get(KNOWN_FINDINGS.SERVER_KEY),
+      textRangeWithHash, lineWithHash,
+      rec.get(KNOWN_FINDINGS.RULE_KEY),
+      rec.get(KNOWN_FINDINGS.MESSAGE),
+      introductionDate
+    );
+  }
+
+  private static LineWithHash getLineWithHash(Record rec) {
+    if (rec.get(KNOWN_FINDINGS.LINE) == null) return null;
+    var line = rec.get(KNOWN_FINDINGS.LINE);
+    var hash = rec.get(KNOWN_FINDINGS.LINE_HASH);
+    return new LineWithHash(line, hash);
+  }
+
+  private static TextRangeWithHash getTextRangeWithHash(Record rec) {
+    if (rec.get(KNOWN_FINDINGS.START_LINE) == null) return null;
+    var startLine = rec.get(KNOWN_FINDINGS.START_LINE);
+    var endLine = rec.get(KNOWN_FINDINGS.END_LINE);
+    var startLineOffset = rec.get(KNOWN_FINDINGS.START_LINE_OFFSET);
+    var endLineOffset = rec.get(KNOWN_FINDINGS.END_LINE_OFFSET);
+    var hash = rec.get(KNOWN_FINDINGS.TEXT_RANGE_HASH);
+    return new TextRangeWithHash(startLine, startLineOffset, endLine, endLineOffset, hash);
+  }
+
+}
---
+++ backend/commons/src/test/java/org/sonarsource/sonarlint/core/commons/storage/repository/KnownFindingsRepositoryTests.java
@@ -0,0 +1,100 @@
+/*
+ * SonarLint Core - Commons
+ * Copyright (C) 2016-2025 SonarSource SA
+ * mailto:info AT sonarsource DOT com
+ *
+ * This program is free software; you can redistribute it and/or
+ * modify it under the terms of the GNU Lesser General Public
+ * License as published by the Free Software Foundation; either
+ * version 3 of the License, or (at your option) any later version.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the GNU
+ * Lesser General Public License for more details.
+ *
+ * You should have received a copy of the GNU Lesser General Public License
+ * along with this program; if not, write to the Free Software Foundation,
+ * Inc., 51 Franklin Street, Fifth Floor, Boston, MA  02110-1301, USA.
+ */
+package org.sonarsource.sonarlint.core.commons.storage.repository;
+
+import java.nio.file.Path;
+import java.time.Instant;
+import java.util.ArrayList;
+import java.util.UUID;
+import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.extension.RegisterExtension;
+import org.junit.jupiter.api.io.TempDir;
+import org.sonarsource.sonarlint.core.commons.KnownFinding;
+import org.sonarsource.sonarlint.core.commons.LineWithHash;
+import org.sonarsource.sonarlint.core.commons.api.TextRangeWithHash;
+import org.sonarsource.sonarlint.core.commons.log.SonarLintLogTester;
+import org.sonarsource.sonarlint.core.commons.storage.SonarLintDatabase;
+import org.sonarsource.sonarlint.core.commons.storage.SonarLintDatabaseInitParams;
+import org.sonarsource.sonarlint.core.commons.storage.SonarLintDatabaseMode;
+
+import static org.assertj.core.api.AssertionsForInterfaceTypes.assertThat;
+
+class KnownFindingsRepositoryTests {
+
+  @RegisterExtension
+  static SonarLintLogTester logTester = new SonarLintLogTester();
+
+  @Test
+  void testKnownFindingsRepository(@TempDir Path temp) {
+    var storageRoot = temp.resolve("storage");
+
+    var db = new SonarLintDatabase(new SonarLintDatabaseInitParams(storageRoot, SonarLintDatabaseMode.FILE));
+    var repo = new KnownFindingsRepository(db);
+
+    var filePath = Path.of("/file/path");
+    var configScopeId = "configScopeId";
+    var issues = new ArrayList<KnownFinding>();
+    var issueUuid1 = UUID.randomUUID();
+    var issueIntroDate1 = Instant.now();
+    var issue1 = new KnownFinding(issueUuid1, "test-message", new TextRangeWithHash(1, 2, 3, 4, "hash1"),
+      new LineWithHash(1, "hash"), "test-issue-rule-1", "Test issue message 1", issueIntroDate1);
+    issues.add(issue1);
+    var issueUuid2 = UUID.randomUUID();
+    var issueIntroDate2 = Instant.now();
+    var issue2 = new KnownFinding(issueUuid2, "test-message", new TextRangeWithHash(5, 6, 7, 8, "hash2"),
+      new LineWithHash(1, "hash"), "test-issue-rule-2", "Test issue message 2", issueIntroDate2);
+    issues.add(issue2);
+    var hotspots = new ArrayList<KnownFinding>();
+    var hotspotUuid1 = UUID.randomUUID();
+    var hotspotIntroDate1 = Instant.now();
+    var hotspot1 = new KnownFinding(hotspotUuid1, "test-message", new TextRangeWithHash(1, 2, 3, 4, "hash1"),
+      new LineWithHash(1, "hash"), "test-hotspot-rule-1", "Test hotspot message 1", hotspotIntroDate1);
+    hotspots.add(hotspot1);
+    var hotspotUuid2 = UUID.randomUUID();
+    var hotspotIntroDate2 = Instant.now();
+    var hotspot2 = new KnownFinding(hotspotUuid2, "test-message", new TextRangeWithHash(5, 6, 7, 8, "hash2"),
+      new LineWithHash(1, "hash"), "test-hotspot-rule-2", "Test hotspot message 2", hotspotIntroDate2);
+    hotspots.add(hotspot2);
+
+    repo.storeKnownIssues(configScopeId, filePath, issues);
+    repo.storeKnownSecurityHotspots(configScopeId, filePath, hotspots);
+
+    var knownIssues = repo.loadIssuesForFile(configScopeId, filePath);
+    var knownHotspots = repo.loadSecurityHotspotsForFile(configScopeId, filePath);
+
+    assertThat(knownIssues).hasSize(2);
+    assertThat(knownHotspots).hasSize(2);
+    var knownIssue = knownIssues.get(0);
+    assertThat(knownIssue.getRuleKey()).isEqualTo(issue1.getRuleKey());
+    assertThat(knownIssue.getServerKey()).isEqualTo(issue1.getServerKey());
+    assertThat(knownIssue.getMessage()).isEqualTo(issue1.getMessage());
+    assertThat(knownIssue.getTextRangeWithHash()).isEqualTo(issue1.getTextRangeWithHash());
+    assertThat(knownIssue.getLineWithHash().getNumber()).isEqualTo(issue1.getLineWithHash().getNumber());
+    assertThat(knownIssue.getLineWithHash().getHash()).isEqualTo(issue1.getLineWithHash().getHash());
+    var knownHotspot = knownHotspots.get(0);
+    assertThat(knownHotspot.getRuleKey()).isEqualTo(hotspot1.getRuleKey());
+    assertThat(knownHotspot.getServerKey()).isEqualTo(hotspot1.getServerKey());
+    assertThat(knownHotspot.getMessage()).isEqualTo(hotspot1.getMessage());
+    assertThat(knownHotspot.getTextRangeWithHash()).isEqualTo(hotspot1.getTextRangeWithHash());
+    assertThat(knownHotspot.getLineWithHash().getNumber()).isEqualTo(hotspot1.getLineWithHash().getNumber());
+    assertThat(knownHotspot.getLineWithHash().getHash()).isEqualTo(hotspot1.getLineWithHash().getHash());
+  }
+
+}
