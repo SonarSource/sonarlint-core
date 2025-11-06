e02bc991bcc9e76db678ac82166a0631b66585a4
Viacheslav Tsuvariev <viacheslav.tsuvariev@sonarsource.com>
2025-11-06 11:17:42 +0100

    SLCORE-1788 SLCORE-1789 SLCORE-1791 Send basic Gessie event

---
+++ backend/telemetry/src/main/java/org/sonarsource/sonarlint/core/telemetry/gessie/GessieService.java
@@ -0,0 +1,62 @@
+/*
+ * SonarLint Core - Telemetry
+ * Copyright (C) 2016-2025 SonarSource SA
+ * mailto:info AT sonarsource DOT com
+ *
+ * This program is free software; you can redistribute it and/or
+ * modify it under the terms of the GNU Lesser General Public
+ * License as published by the Free Software Foundation; either
+ * version 3 of the License, or (at your option) any later version.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the GNU
+ * Lesser General Public License for more details.
+ *
+ * You should have received a copy of the GNU Lesser General Public License
+ * along with this program; if not, write to the Free Software Foundation,
+ * Inc., 51 Franklin Street, Fifth Floor, Boston, MA  02110-1301, USA.
+ */
+package org.sonarsource.sonarlint.core.telemetry.gessie;
+
+import jakarta.annotation.PostConstruct;
+import java.time.Instant;
+import java.util.UUID;
+import org.sonarsource.sonarlint.core.rpc.protocol.backend.initialize.InitializeParams;
+import org.sonarsource.sonarlint.core.rpc.protocol.backend.initialize.TelemetryClientConstantAttributesDto;
+import org.sonarsource.sonarlint.core.telemetry.common.TelemetryUserSetting;
+import org.sonarsource.sonarlint.core.telemetry.gessie.event.GessieEvent;
+import org.sonarsource.sonarlint.core.telemetry.gessie.event.GessieMetadata;
+import org.sonarsource.sonarlint.core.telemetry.gessie.event.payload.MessagePayload;
+
+import static org.sonarsource.sonarlint.core.rpc.protocol.backend.initialize.BackendCapability.GESSIE_TELEMETRY;
+import static org.sonarsource.sonarlint.core.telemetry.gessie.event.GessieMetadata.SonarLintDomain;
+
+public class GessieService {
+
+  private final boolean isGessieFeatureEnabled;
+  private final TelemetryClientConstantAttributesDto telemetryConstantAttributes;
+  private final GessieHttpClient client;
+  private final TelemetryUserSetting userSetting;
+
+  public GessieService(InitializeParams initializeParams, GessieHttpClient client, TelemetryUserSetting userSetting) {
+    this.isGessieFeatureEnabled = initializeParams.getBackendCapabilities().contains(GESSIE_TELEMETRY);
+    this.telemetryConstantAttributes = initializeParams.getTelemetryConstantAttributes();
+    this.client = client;
+    this.userSetting = userSetting;
+  }
+
+  @PostConstruct
+  public void onStartup() {
+    if (isGessieFeatureEnabled && userSetting.isTelemetryEnabledByUser()) {
+      client.postEvent(new GessieEvent(
+        new GessieMetadata(UUID.randomUUID(),
+          new GessieMetadata.GessieSource(SonarLintDomain.fromProductKey(telemetryConstantAttributes.getProductKey())),
+          "Analytics.Editor.PluginActivated",
+          Long.toString(Instant.now().toEpochMilli()),
+          "0"),
+        new MessagePayload("Gessie integration test event", "slcore_start")
+      ));
+    }
+  }
+}
---
+++ backend/telemetry/src/test/java/org/sonarsource/sonarlint/core/telemetry/gessie/GessieHttpClientTests.java
@@ -0,0 +1,119 @@
+/*
+ * SonarLint Core - Telemetry
+ * Copyright (C) 2016-2025 SonarSource SA
+ * mailto:info AT sonarsource DOT com
+ *
+ * This program is free software; you can redistribute it and/or
+ * modify it under the terms of the GNU Lesser General Public
+ * License as published by the Free Software Foundation; either
+ * version 3 of the License, or (at your option) any later version.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the GNU
+ * Lesser General Public License for more details.
+ *
+ * You should have received a copy of the GNU Lesser General Public License
+ * along with this program; if not, write to the Free Software Foundation,
+ * Inc., 51 Franklin Street, Fifth Floor, Boston, MA  02110-1301, USA.
+ */
+package org.sonarsource.sonarlint.core.telemetry.gessie;
+
+import com.github.tomakehurst.wiremock.junit5.WireMockExtension;
+import com.github.tomakehurst.wiremock.matching.EqualToPattern;
+import java.io.IOException;
+import java.net.URISyntaxException;
+import java.nio.file.Files;
+import java.nio.file.Path;
+import java.util.Objects;
+import java.util.UUID;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.extension.RegisterExtension;
+import org.sonarsource.sonarlint.core.http.HttpClientProvider;
+import org.sonarsource.sonarlint.core.telemetry.gessie.event.GessieEvent;
+import org.sonarsource.sonarlint.core.telemetry.gessie.event.GessieMetadata;
+import org.sonarsource.sonarlint.core.telemetry.gessie.event.payload.MessagePayload;
+
+import static com.github.tomakehurst.wiremock.client.WireMock.aResponse;
+import static com.github.tomakehurst.wiremock.client.WireMock.equalToJson;
+import static com.github.tomakehurst.wiremock.client.WireMock.post;
+import static com.github.tomakehurst.wiremock.client.WireMock.postRequestedFor;
+import static com.github.tomakehurst.wiremock.client.WireMock.urlEqualTo;
+import static com.github.tomakehurst.wiremock.core.WireMockConfiguration.wireMockConfig;
+import static org.awaitility.Awaitility.await;
+import static org.sonarsource.sonarlint.core.telemetry.gessie.event.GessieMetadata.GessieSource;
+import static org.sonarsource.sonarlint.core.telemetry.gessie.event.GessieMetadata.SonarLintDomain;
+
+class GessieHttpClientTests {
+
+  private static final String IDE_ENDPOINT = "/ide";
+
+  private GessieHttpClient tested;
+
+  @RegisterExtension
+  static WireMockExtension mockGessie = WireMockExtension.newInstance()
+    .options(wireMockConfig().dynamicPort())
+    .build();
+
+  @BeforeEach
+  void setUp() {
+    tested = new GessieHttpClient(HttpClientProvider.forTesting(), mockGessie.baseUrl());
+  }
+
+  @Test
+  void should_upload_accepted_payload() throws URISyntaxException, IOException {
+    mockGessie.stubFor(post(IDE_ENDPOINT)
+      .willReturn(aResponse().withStatus(202)));
+
+    tested.postEvent(getPayload());
+
+    var fileContent = getTestJson("GessieRequest");
+    await().untilAsserted(() -> mockGessie.verify(postRequestedFor(urlEqualTo(IDE_ENDPOINT))
+        .withHeader("x-api-key", new EqualToPattern("value"))
+      .withRequestBody(equalToJson(fileContent))));
+  }
+
+  @Test
+  void should_not_retry_400_error() throws URISyntaxException, IOException {
+    mockGessie.stubFor(post(IDE_ENDPOINT)
+      .willReturn(aResponse().withStatus(400)));
+
+    tested.postEvent(new GessieEvent(null, null));
+
+    var invalidRequest = getTestJson("InvalidRequest");
+    await().untilAsserted(() -> mockGessie.verify(postRequestedFor(urlEqualTo(IDE_ENDPOINT))
+      .withHeader("x-api-key", new EqualToPattern("value"))
+      .withRequestBody(equalToJson(invalidRequest))));
+  }
+
+  @Test
+  void should_not_retry_403_error() throws URISyntaxException, IOException {
+    mockGessie.stubFor(post(IDE_ENDPOINT)
+      .willReturn(aResponse().withStatus(403)));
+
+    tested.postEvent(getPayload());
+
+    var fileContent = getTestJson("GessieRequest");
+    await().untilAsserted(() -> mockGessie.verify(postRequestedFor(urlEqualTo(IDE_ENDPOINT))
+      .withHeader("x-api-key", new EqualToPattern("value"))
+      .withRequestBody(equalToJson(fileContent))));
+  }
+
+  private String getTestJson(String fileName) throws URISyntaxException, IOException {
+    var resource = Objects.requireNonNull(getClass().getResource("/response/gessie/GessieHttpClientTest/" + fileName + ".json"))
+      .toURI();
+    return Files.readString(Path.of(resource));
+  }
+
+  private static GessieEvent getPayload() {
+    return new GessieEvent(
+      new GessieMetadata(UUID.fromString("a36e25e8-5a92-4b5d-93b4-ba0045947b4c"),
+        new GessieSource(SonarLintDomain.INTELLIJ),
+        "Analytics.Test.TestEvent",
+        "1761821877867",
+        "0"),
+      new MessagePayload("Test event", "test")
+    );
+  }
+}
