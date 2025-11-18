3c5a57071ed8752787d4e0aeaa518cb7c4e31466
Viacheslav Tsuvariev <viacheslav.tsuvariev@sonarsource.com>
2025-11-18 11:52:40 +0100

    SLCORE-1844 Remote URL accepted binding suggestions measure is wrong

--- backend/telemetry/src/main/java/org/sonarsource/sonarlint/core/telemetry/measures/payload/TelemetryMeasuresBuilder.java
+++ backend/telemetry/src/main/java/org/sonarsource/sonarlint/core/telemetry/measures/payload/TelemetryMeasuresBuilder.java
@@ -119,7 +119,7 @@ public class TelemetryMeasuresBuilder {
   private void addNewBindingsMeasures(ArrayList<TelemetryMeasuresValue> values) {
     if (liveAttributes.usesConnectedMode()) {
       values.add(new TelemetryMeasuresValue("new_bindings.manual", String.valueOf(storage.getManualAddedBindingsCount()), INTEGER, DAILY));
-      values.add(new TelemetryMeasuresValue("new_bindings.accepted_suggestion_remote_url", String.valueOf(storage.getSuggestedRemoteBindingsCount()), INTEGER, DAILY));
+      values.add(new TelemetryMeasuresValue("new_bindings.accepted_suggestion_remote_url", String.valueOf(storage.getNewBindingsRemoteUrlCount()), INTEGER, DAILY));
       values.add(new TelemetryMeasuresValue("new_bindings.accepted_suggestion_properties_file", String.valueOf(storage.getNewBindingsPropertiesFileCount()), INTEGER, DAILY));
       values.add(new TelemetryMeasuresValue("new_bindings.accepted_suggestion_shared_config_file",
         String.valueOf(storage.getNewBindingsSharedConfigurationCount()), INTEGER, DAILY));
