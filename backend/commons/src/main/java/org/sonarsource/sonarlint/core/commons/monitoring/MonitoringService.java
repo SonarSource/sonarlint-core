48d590c8507f7bb4823404bcc89e9477782869d3
Eray Felek <eray.felek@sonarsource.com>
2025-11-26 15:34:20 +0100

    SLCORE-1861 Start collecting uncaught exceptions in production

--- backend/commons/src/main/java/org/sonarsource/sonarlint/core/commons/monitoring/MonitoringService.java
+++ backend/commons/src/main/java/org/sonarsource/sonarlint/core/commons/monitoring/MonitoringService.java
@@ -38,12 +38,12 @@ public class MonitoringService {
   private static final String DSN_DEFAULT = "https://ad1c1fe3cb2b12fc2d191ecd25f89866@o1316750.ingest.us.sentry.io/4508201175089152";
 
   public static final String TRACES_SAMPLE_RATE_PROPERTY = "sonarlint.internal.monitoring.tracesSampleRate";
-  private static final double TRACES_SAMPLE_RATE_DEFAULT = 0.0001D;
+  private static final double TRACES_SAMPLE_RATE_DEFAULT = 0D;
   private static final double TRACES_SAMPLE_RATE_DOGFOOD_DEFAULT = 0.01D;
   private static final double TRACES_SAMPLE_RATE_FLIGHT_RECORDER = 1D;
 
   private static final String ENVIRONMENT_FLIGHT_RECORDER = "flight_recorder";
-  private static final String ENVIRONMENT_DOGFOOD = "dogfood";
+  private static final String ENVIRONMENT_PRODUCTION = "production";
 
   private static final SonarLintLogger LOG = SonarLintLogger.get();
 
@@ -66,19 +66,13 @@ public class MonitoringService {
       return;
     }
 
-    if (shouldInitializeSentry()) {
-      var sentryConfiguration = getSentryConfiguration();
-      LOG.info("Initializing Sentry");
-      Sentry.init(sentryConfiguration);
-      active = true;
-      if (initializeParams.flightRecorderEnabled()) {
-        configureFlightRecorderSession();
-      }
+    var sentryConfiguration = getSentryConfiguration();
+    LOG.info("Initializing Sentry");
+    Sentry.init(sentryConfiguration);
+    active = true;
+    if (initializeParams.flightRecorderEnabled()) {
+      configureFlightRecorderSession();
     }
-  }
-
-  private boolean shouldInitializeSentry() {
-    return dogfoodEnvDetectionService.isDogfoodEnvironment() || initializeParams.flightRecorderEnabled();
   }
 
   public boolean isActive() {
@@ -116,7 +110,7 @@ public class MonitoringService {
     if (initializeParams.flightRecorderEnabled()) {
       return ENVIRONMENT_FLIGHT_RECORDER;
     }
-    return ENVIRONMENT_DOGFOOD;
+    return ENVIRONMENT_PRODUCTION;
   }
 
   private static <T extends SentryBaseEvent> T beforeSend(T event, Hint hint) {
