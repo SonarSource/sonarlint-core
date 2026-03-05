e31db66887d95e94355acd3d30bc5ccc151f4def
Nicolas Quinquenel <nicolas.quinquenel@sonarsource.com>
2026-03-05 15:20:34 +0100

    Fix analyzers

--- medium-tests/src/test/java/utils/PluginLocator.java
+++ medium-tests/src/test/java/utils/PluginLocator.java
@@ -24,45 +24,45 @@ import java.nio.file.Path;
 import java.nio.file.Paths;
 
 public class PluginLocator {
-  public static final String SONAR_JAVA_PLUGIN_VERSION = "8.22.0.41895";
+  public static final String SONAR_JAVA_PLUGIN_VERSION = "8.25.0.42802";
   public static final String SONAR_JAVA_PLUGIN_JAR = "sonar-java-plugin-" + SONAR_JAVA_PLUGIN_VERSION + ".jar";
   public static final String SONAR_JAVA_PLUGIN_JAR_HASH = "fa425ffda3272aef1abc137941a64772";
-  public static final String SONAR_JAVA_SE_PLUGIN_VERSION = "8.19.0.1586";
+  public static final String SONAR_JAVA_SE_PLUGIN_VERSION = "8.25.0.42802";
   public static final String SONAR_JAVA_SE_PLUGIN_JAR = "sonar-java-symbolic-execution-plugin-" + SONAR_JAVA_SE_PLUGIN_VERSION + ".jar";
   public static final String SONAR_JAVA_SE_PLUGIN_JAR_HASH = "unused";
 
-  public static final String SONAR_DBD_PLUGIN_VERSION = "2.5.0.19957";
+  public static final String SONAR_DBD_PLUGIN_VERSION = "2.7.0.20531";
   public static final String SONAR_DBD_PLUGIN_JAR = "sonar-dbd-plugin-" + SONAR_DBD_PLUGIN_VERSION + ".jar";
   public static final String SONAR_DBD_PLUGIN_JAR_HASH = "unused";
   public static final String SONAR_DBD_JAVA_PLUGIN_VERSION = SONAR_DBD_PLUGIN_VERSION;
   public static final String SONAR_DBD_JAVA_PLUGIN_JAR = "sonar-dbd-java-frontend-plugin-" + SONAR_DBD_JAVA_PLUGIN_VERSION + ".jar";
   public static final String SONAR_DBD_JAVA_PLUGIN_JAR_HASH = "unused";
 
-  public static final String SONAR_JAVASCRIPT_PLUGIN_VERSION = "11.7.1.36988";
+  public static final String SONAR_JAVASCRIPT_PLUGIN_VERSION = "11.8.0.37897";
   public static final String SONAR_JAVASCRIPT_PLUGIN_JAR = "sonar-javascript-plugin-" + SONAR_JAVASCRIPT_PLUGIN_VERSION + ".jar";
   public static final String SONAR_JAVASCRIPT_PLUGIN_JAR_HASH = "2fab92be44e07f1d367f891a55258736";
 
-  public static final String SONAR_PHP_PLUGIN_VERSION = "3.42.2.15271";
+  public static final String SONAR_PHP_PLUGIN_VERSION = "3.55.0.15704";
   public static final String SONAR_PHP_PLUGIN_JAR = "sonar-php-plugin-" + SONAR_PHP_PLUGIN_VERSION + ".jar";
   public static final String SONAR_PHP_PLUGIN_JAR_HASH = "88ddaa391f3176891a62375e98b76ae9";
 
-  public static final String SONAR_PYTHON_PLUGIN_VERSION = "5.14.2.29072";
+  public static final String SONAR_PYTHON_PLUGIN_VERSION = "5.18.0.31561";
   public static final String SONAR_PYTHON_PLUGIN_JAR = "sonar-python-plugin-" + SONAR_PYTHON_PLUGIN_VERSION + ".jar";
   public static final String SONAR_PYTHON_PLUGIN_JAR_HASH = "e1cff9e38811ab71e6efbff087743367";
 
-  public static final String SONAR_KOTLIN_PLUGIN_VERSION = "3.3.1.7792";
+  public static final String SONAR_KOTLIN_PLUGIN_VERSION = "3.4.0.8957";
   public static final String SONAR_KOTLIN_PLUGIN_JAR = "sonar-kotlin-plugin-" + SONAR_KOTLIN_PLUGIN_VERSION + ".jar";
   public static final String SONAR_KOTLIN_PLUGIN_JAR_HASH = "XXX";
 
-  public static final String SONAR_XML_PLUGIN_VERSION = "2.14.2.7437";
+  public static final String SONAR_XML_PLUGIN_VERSION = "2.16.0.7616";
   public static final String SONAR_XML_PLUGIN_JAR = "sonar-xml-plugin-" + SONAR_XML_PLUGIN_VERSION + ".jar";
   public static final String SONAR_XML_PLUGIN_JAR_HASH = "XXX";
 
-  public static final String SONAR_TEXT_PLUGIN_VERSION = "2.34.0.9939";
+  public static final String SONAR_TEXT_PLUGIN_VERSION = "2.41.0.10709";
   public static final String SONAR_TEXT_PLUGIN_JAR = "sonar-text-plugin-" + SONAR_TEXT_PLUGIN_VERSION + ".jar";
   public static final String SONAR_TEXT_PLUGIN_JAR_HASH = "f679af4c0e2992c3cec281d6a9cd5062";
 
-  public static final String SONAR_CFAMILY_PLUGIN_VERSION = "6.75.1.93101";
+  public static final String SONAR_CFAMILY_PLUGIN_VERSION = "6.78.0.96395";
   private static final String SONAR_CFAMILY_PLUGIN_JAR = "sonar-cfamily-plugin-" + SONAR_CFAMILY_PLUGIN_VERSION + ".jar";
   public static final String SONAR_CFAMILY_PLUGIN_JAR_HASH = "XXX";
 
