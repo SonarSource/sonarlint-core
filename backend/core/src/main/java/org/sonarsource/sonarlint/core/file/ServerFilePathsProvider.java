04a1e80d339e1639917687e0bdf03860cace6afd
Manuel Vianello <manuel.vianello@sonarsource.com>
2026-03-09 16:57:18 +0100

    Update ServerFilePathsProvider

--- backend/core/src/main/java/org/sonarsource/sonarlint/core/file/ServerFilePathsProvider.java
+++ backend/core/src/main/java/org/sonarsource/sonarlint/core/file/ServerFilePathsProvider.java
@@ -105,6 +105,7 @@ public class ServerFilePathsProvider {
         return paths;
       });
     } catch (CancellationException e) {
+      LOG.error("Test 123", e);
       throw e;
     } catch (Exception e) {
       LOG.debug("Error while getting server file paths for project '{}'", binding.sonarProjectKey(), e);
