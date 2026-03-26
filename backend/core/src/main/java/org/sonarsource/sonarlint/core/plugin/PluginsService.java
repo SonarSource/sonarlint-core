21246baf67da496bb4811ae7558d6fa2208c1d8b
Nicolas Quinquenel <nicolas.quinquenel@sonarsource.com>
2026-03-26 15:38:40 +0100

    Rebase and PR review

--- backend/core/src/main/java/org/sonarsource/sonarlint/core/plugin/PluginsService.java
+++ backend/core/src/main/java/org/sonarsource/sonarlint/core/plugin/PluginsService.java
@@ -28,6 +28,7 @@ import java.util.HashMap;
 import java.util.HashSet;
 import java.util.List;
 import java.util.Map;
+import java.util.Optional;
 import java.util.Set;
 import javax.annotation.Nullable;
 import org.jetbrains.annotations.NotNull;
