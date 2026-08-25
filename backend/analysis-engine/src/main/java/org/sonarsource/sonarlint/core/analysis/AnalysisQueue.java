734a2ac1957e911a9a21226de486d6ef9a9a55b3
tomshafir-sonarsource <tom.shafir@sonarsource.com>
2026-08-25 10:01:48 +0200

    Prevent overflows in comparators

--- backend/analysis-engine/src/main/java/org/sonarsource/sonarlint/core/analysis/AnalysisQueue.java
+++ backend/analysis-engine/src/main/java/org/sonarsource/sonarlint/core/analysis/AnalysisQueue.java
@@ -134,7 +134,7 @@ public class AnalysisQueue {
       }
       LOG.debug("Batching {} analyses", removedCommands.size() + 1);
       return Stream.concat(Stream.of(analyzeCommand), removedCommands.stream())
-        .sorted((c1, c2) -> (int) (c1.getSequenceNumber() - c2.getSequenceNumber()))
+        .sorted((c1, c2) -> Long.compare(c1.getSequenceNumber(), c2.getSequenceNumber()))
         .reduce(AnalyzeCommand::mergeWith)
         // this last clause should never occur
         .orElse(analyzeCommand);
