e80e3917b802c5d05473fdf3679acac38bb46cfc
Nicolas Quinquenel <nicolas.quinquenel@sonarsource.com>
2026-09-24 11:01:23 +0200

    SLCORE-2585 Add AI integration telemetry

---
+++ rpc-protocol/src/main/java/org/sonarsource/sonarlint/core/rpc/protocol/client/telemetry/AiIntegrationCliStateObservedParams.java
@@ -0,0 +1,74 @@
+/*
+ * SonarLint Core - RPC Protocol
+ * Copyright (C) SonarSource Sàrl
+ * mailto:info AT sonarsource DOT com
+ *
+ * This program is free software; you can redistribute it and/or
+ * modify it under the terms of the GNU Lesser General Public
+ * License as published by the Free Software Foundation; either
+ * version 3 of the License, or (at your option) any later version.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the GNU
+ * Lesser General Public License for more details.
+ *
+ * You should have received a copy of the GNU Lesser General Public License
+ * along with this program; if not, write to the Free Software Foundation,
+ * Inc., 51 Franklin Street, Fifth Floor, Boston, MA  02110-1301, USA.
+ */
+package org.sonarsource.sonarlint.core.rpc.protocol.client.telemetry;
+
+import com.google.gson.annotations.JsonAdapter;
+import org.sonarsource.sonarlint.core.rpc.protocol.adapter.StrictBooleanTypeAdapter;
+import org.sonarsource.sonarlint.core.rpc.protocol.backend.ai.AiIntegrationHost;
+import org.sonarsource.sonarlint.core.rpc.protocol.backend.ai.CliAuthenticationStatus;
+import org.sonarsource.sonarlint.core.rpc.protocol.backend.ai.CliInstallationStatus;
+
+public class AiIntegrationCliStateObservedParams {
+  private final AiIntegrationObservationTrigger trigger;
+  private final CliInstallationStatus installationStatus;
+  private final CliAuthenticationStatus authenticationStatus;
+  @JsonAdapter(StrictBooleanTypeAdapter.class)
+  private final Boolean vortexAvailable;
+  private final AiIntegrationHost host;
+  private final AiIntegrationEnvironment environment;
+
+  public AiIntegrationCliStateObservedParams(AiIntegrationObservationTrigger trigger,
+    CliInstallationStatus installationStatus,
+    CliAuthenticationStatus authenticationStatus,
+    Boolean vortexAvailable,
+    AiIntegrationHost host,
+    AiIntegrationEnvironment environment) {
+    this.trigger = trigger;
+    this.installationStatus = installationStatus;
+    this.authenticationStatus = authenticationStatus;
+    this.vortexAvailable = vortexAvailable;
+    this.host = host;
+    this.environment = environment;
+  }
+
+  public AiIntegrationObservationTrigger getTrigger() {
+    return trigger;
+  }
+
+  public CliInstallationStatus getInstallationStatus() {
+    return installationStatus;
+  }
+
+  public CliAuthenticationStatus getAuthenticationStatus() {
+    return authenticationStatus;
+  }
+
+  public Boolean getVortexAvailable() {
+    return vortexAvailable;
+  }
+
+  public AiIntegrationHost getHost() {
+    return host;
+  }
+
+  public AiIntegrationEnvironment getEnvironment() {
+    return environment;
+  }
+}
