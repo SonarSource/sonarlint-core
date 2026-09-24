9a8c6277ac87b93e9b11983383ce8fabe5f66924
Damien Urruty <damien.urruty@sonarsource.com>
2026-09-24 09:48:52 +0200

    SLCORE-2594 Drop redundant default atMost from awaitTelemetryStatus

--- medium-tests/src/test/java/mediumtest/monitoring/MonitoringMediumTests.java
+++ medium-tests/src/test/java/mediumtest/monitoring/MonitoringMediumTests.java
@@ -576,10 +576,9 @@ class MonitoringMediumTests {
   }
 
   private static void awaitTelemetryStatus(SonarLintTestRpcServer backend, boolean enabled) {
-    await().atMost(10, TimeUnit.SECONDS)
-      .untilAsserted(() -> {
-        assertThat(backend.getTelemetryService().getStatus().join().isEnabled()).isEqualTo(enabled);
-        assertThat(Sentry.isEnabled()).isEqualTo(enabled);
-      });
+    await().untilAsserted(() -> {
+      assertThat(backend.getTelemetryService().getStatus().join().isEnabled()).isEqualTo(enabled);
+      assertThat(Sentry.isEnabled()).isEqualTo(enabled);
+    });
   }
 }
